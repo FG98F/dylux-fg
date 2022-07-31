c9362ed4bc21b4e823262f2eba4964102465b932
FG98 <81941690+FG98F@users.noreply.github.com>
2022-07-31 10:59:02 -0400

    Update handler.js

--- handler.js
+++ handler.js
@@ -11,7 +11,9 @@ import Helper from './lib/helper.js'
 import db, { loadDatabase } from './lib/database.js'
 import Queque from './lib/queque.js'
 
-// const { proto } = (await import('@adiwajshing/baileys')).default
+/** @type {import('@adiwajshing/baileys')} */
+const { getContentType, proto } = (await import('@adiwajshing/baileys')).default
+
 const isNumber = x => typeof x === 'number' && !isNaN(x)
 
 /**
@@ -669,34 +671,38 @@ export async function groupsUpdate(groupsUpdate) {
  * @param {import('@adiwajshing/baileys').BaileysEventMap<unknown>['messages.delete']} message 
  */
 export async function deleteUpdate(message) {
-    if (message.keys && Array.isArray(message.keys)) {
-        try {
-            for (const key of message.keys) {
-                if (key.fromMe) continue
-                const msg = Connection.store.loadMessage(key.id)
-                if (!msg) continue
-                let chat = db.data.chats[msg.key.remoteJid]
-                if (!chat || chat.delete) continue
-                const participant = msg.participant || msg.key.participant || msg.key.remoteJid
-                
-await this.reply(msg.chat, `
-≡ Borró un mensaje  
-┌─⊷  𝘼𝙉𝙏𝙄 𝘿𝙀𝙇𝙀𝙏𝙀 
+
+    if (Array.isArray(message.keys) && message.keys.length > 0) {
+        const tasks = await Promise.allSettled(message.keys.map(async (key) => {
+            if (key.fromMe) return
+            const msg = this.loadMessage(key.remoteJid, key.id) || this.loadMessage(key.id)
+            if (!msg || !msg.message) return
+            let chat = db.data.chats[key.remoteJid]
+            if (!chat || chat.delete) return
+
+            // if message type is conversation, convert it to extended text message because if not, it will throw an error
+            const mtype = getContentType(msg.message)
+            if (mtype === 'conversation') {
+                msg.message.extendedTextMessage = { text: msg.message[mtype] }
+                delete msg.message[mtype]
+            }
+
+            const participant = msg.participant || msg.key.participant || msg.key.remoteJid
+
+            await this.reply(key.remoteJid, `
+           ≡ Borró un mensaje  
+┌─⊷  𝘼𝙉𝙏𝙄 𝘿𝙀𝙇𝙀𝙏𝙀
 ▢ *Nombre :* @${participant.split`@`[0]} 
 └─────────────
 
 Para desactivar esta función, escriba 
 */off antidelete*
-`.trim(), msg, {
-                    mentions: [participant]
-                })
-                this.copyNForward(msg.key.remoteJid, msg).catch(e => console.log(e, msg))
-            }
-        } catch (e) {
-            console.error(e)
-        }
+`.trim(), msg, { mentions: [participant] })
+            return await this.copyNForward(key.remoteJid, msg).catch(e => console.log(e, msg))
+        }))
+        tasks.map(t => t.status === 'rejected' && console.error(t.reason))
     }
-}
+} 
 
 
 global.dfail = (type, m, conn) => {
