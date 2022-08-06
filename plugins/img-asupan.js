997f5bb3f10427f559c4f6259386190ea489f663
FG98 <81941690+FG98F@users.noreply.github.com>
2022-08-06 11:01:52 -0400

    Update and rename prem-asupan.js to img-asupan.js

--- plugins/img-asupan.js
+++ plugins/img-asupan.js
@@ -9,9 +9,10 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 	let asupan = (await axios.get(`https://raw.githubusercontent.com/FG98F/team-fg/main/img/${asufg}.json`)).data
   
 await conn.sendHydrated(m.chat, `✅ Aquí tiene`, igfg, pickRandom(asupan), null, null, null, null, [['▷▷ SIGUIENTE', `${usedPrefix + command}`]], m)
+m.react(dmoji)
 }
 handler.help = ['asupan']
-handler.tags = ['premium']
+handler.tags = ['img']
 handler.command = ['asupan']
 handler.premium = false
 handler.limit = true
