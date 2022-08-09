e6ad35d16d54870002f441ab0bbe444c4cd3135d
FG98 <81941690+FG98F@users.noreply.github.com>
2022-08-09 19:04:48 -0400

    Update tools-ssweb.js

--- plugins/tools-ssweb.js
+++ plugins/tools-ssweb.js
@@ -1,13 +1,37 @@
+/*
 import fetch from 'node-fetch'
 let handler = async (m, { conn, command, args }) => {
 let full = /f$/i.test(command)
 if (!args[0]) return m.reply('✳️ Ingrese la Url de una página')
+m.react(rwait) 
 let url = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
 let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url, full }))).buffer()
 conn.sendFile(m.chat, ss, 'screenshot.png', '✅ Captura de la Pagina', m)
+m.react(done) 
 }
 handler.help = ['ssweb <url>']
 handler.tags = ['tools']
 handler.command = ['ssweb', 'ss', 'captura'] 
+
+export default handler
+*/
+
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, command, args }) => {
+if (!args[0]) return m.reply('✳️ Ingrese la Url de una página')
+    m.react(rwait) 
+	let ss = await (await fetch(`https://shot.screenshotapi.net/screenshot?url=${args[0]}&full_page=true&fresh=true&output=image&file_type=png&wait_for_event=load`)).buffer() 
+    conn.sendFile(m.chat, ss, 'ssweb.png', '✅ Captura de la Pagina', m)
+   
+   m.react(done) 
+
+}
+handler.help = ['ssweb <url>']
+handler.tags = ['tools']
+handler.command = ['ssweb', 'ss', 'captura'] 
+
+handler.limit = true
+handler.fail = null
 
 export default handler
