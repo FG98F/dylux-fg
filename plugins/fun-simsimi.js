aa1b1edc18ce3956376af204620fd8ebdbdfc191
FG98 <81941690+FG98F@users.noreply.github.com>
2022-08-10 09:53:55 -0400

    Update fun-simsimi.js

--- plugins/fun-simsimi.js
+++ plugins/fun-simsimi.js
@@ -1,18 +1,15 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-	let name = conn.getName(m.sender)
+	
+ let name = conn.getName(m.sender)
   if (!text) throw `Hola *${name}* Quieres charlar un rato? \nResponde con *${usedPrefix + command}* (tu mensaje) \n\n📌 Ejemplo : *${usedPrefix + command}* Hola bot`
-  try {
-  let res = await fetch(`https://api-sv2.simsimi.net/v2/?text=${text}&lc=es`)
+  m.react('🗣️') 
+  let res = await fetch(global.API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(text), lc: "es" }, ''))
   let json = await res.json()
-  let tes = json.success.replace('simsimi', 'DyLux').replace('Simsimi', 'DyLux').replace('sim simi', 'DyLux')
-    m.reply(`${tes}`) 
-  } catch {
-    m.reply('😔 No entiendo')
-  }
+  if (json.success) m.reply(json.success.replace('simsimi', 'DyLux').replace('Simsimi', 'DyLux').replace('sim simi', 'DyLux'))
+  else throw json
 }
-
 handler.help = ['bot']
 handler.tags = ['main']
 handler.command = ['bot', 'simi'] 
