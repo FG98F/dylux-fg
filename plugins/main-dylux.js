dae18e74b05c166b1514536f3dac7d112941e5d7
FG98 <81941690+FG98F@users.noreply.github.com>
2022-08-10 10:31:25 -0400

    Update and rename gp-dylux.js to main-dylux.js

---
+++ plugins/main-dylux.js
@@ -0,0 +1,32 @@
+let handler = async m => m.reply(`
+
+≡  *DyLux  ┃ ᴮᴼᵀ*   GRUPOS
+
+─────────────
+▢ Grupo 1
+https://chat.whatsapp.com/IO9jmpI72ejHiN4unRZleU
+
+▢ Grupo 2
+https://chat.whatsapp.com/CDUqNRu5Kh5KY5uqQI0BKE
+
+▢ Grupo 3
+https://chat.whatsapp.com/FSVDTytQxnTIPx8t8wBWeM
+─────────────
+≡ Enlaces anulados? entre aquí! 
+
+▢ Grupos WhatsApp 
+ https://instabio.cc/fg98ff
+─────────────
+▢ *Grupo Telegram*
+ https://t.me/fgawgp
+
+▢ *YouTube*
+• https://www.youtube.com/fg98f
+
+
+`.trim())
+handler.help = ['gpdylux']
+handler.tags = ['main']
+handler.command = ['grupos', 'groupdylux', 'dxgp', 'dygp', 'gpdylux'] 
+
+export default handler
