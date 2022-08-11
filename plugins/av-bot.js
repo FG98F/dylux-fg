22efe4b0efaa1cb90577990d8c49a502aa3c2bdb
FG98 <81941690+FG98F@users.noreply.github.com>
2022-08-11 09:44:59 -0400

    Update av-bot.js

--- plugins/av-bot.js
+++ plugins/av-bot.js
@@ -6,10 +6,10 @@ let handler = async (m, { conn}) => {
 let name = conn.getName(m.sender)
 
 let fgac = ["criss", "andrea"]
-let nfg = fgac[Math.floor(Math.random() * fgac.length)] 
+let nfg = pickRandom(fgac)
 let av = `./src/mp3/${nfg}.mp3`
 
-conn.sendHydrated(m.chat, `Hola *${name}* \n \nNecesitas ayuda? \n`, igfg, '', 'https://youtube.com/fg98f', 'YouTube', null, null, [
+conn.sendButton(m.chat, `Hola *${name}* \n \nNecesitas ayuda? \n`, igfg, null, [
       ['⦙☰ Menu', '/help'],
       ['⦙☰ Menu 2', '/menu2'],
       ['⌬ Grupos', '/gpdylux']
@@ -21,4 +21,9 @@ ptt: true
 }
 handler.customPrefix = /^(bot|dylux)$/i
 handler.command = new RegExp
+
 export default handler
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
