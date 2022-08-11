dc859082fd27c3c76ca5ed3124f0771529385c28
FG98 <81941690+FG98F@users.noreply.github.com>
2022-08-11 09:16:20 -0400

    Update img-asupan.js

--- plugins/img-asupan.js
+++ plugins/img-asupan.js
@@ -4,13 +4,12 @@ import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command }) => {
 	
 	let asu = ["asupan", "asupan-la"]
-    let asufg = asu[Math.floor(Math.random() * asu.length)]
-	
+    let asufg = pickRandom(asu)
 	let asupan = (await axios.get(`https://raw.githubusercontent.com/FG98F/team-fg/main/img/${asufg}.json`)).data
-  
-await conn.sendHydrated(m.chat, `✅ Aquí tiene`, igfg, pickRandom(asupan), null, null, null, null, [['▷▷ SIGUIENTE', `${usedPrefix + command}`]], m)
-m.react(dmoji)
+    conn.sendButton(m.chat, `✅ Resultado`, igfg, pickRandom(asupan), [['▷▷ SIGUIENTE', `${usedPrefix + command}`]],m)
+    m.react(dmoji)
 }
+
 handler.help = ['asupan']
 handler.tags = ['img']
 handler.command = ['asupan']
