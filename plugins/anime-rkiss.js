baef88945775a58c6ba26df2c916568761f5c22c
FG98 <81941690+FG98F@users.noreply.github.com>
2022-08-11 11:12:18 -0400

    Create anime-rkiss.js

---
+++ plugins/anime-rkiss.js
@@ -0,0 +1,34 @@
+import fetch from 'node-fetch'
+import axios from 'axios'
+import { sticker } from '../lib/sticker.js'
+import MessageType from '@adiwajshing/baileys'
+import db from '../lib/database.js'
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+	
+	 let who
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
+    else who = m.chat
+    let user = db.data.users[who]
+    let name = conn.getName(who) 
+   let name2 = conn.getName(m.sender) 
+    if (!who) throw `✳️ Etiqueta o menciona a alguien\n\n📌 Ejemplo : ${usedPrefix + command} @tag`
+   m.react(rwait)
+
+  let rki = await fetch(`https://api.waifu.pics/sfw/kiss`)
+    if (!rki.ok) throw await rki.text()
+   let jkis = await rki.json()
+   let { url } = jkis
+   let stiker = await sticker(null, url, `(${name2}) le dio un beso a`, `${name}`)
+   conn.sendFile(m.chat, stiker, null, { asSticker: true }, m)
+   m.react('😚') 
+   
+}
+
+handler.help = ['kiss @tag']
+handler.tags = ['rnime']
+handler.command = /^(kiss|beso)$/i
+handler.limit = true
+handler.group = true
+
+export default handler
