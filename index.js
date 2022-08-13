99ecaabd9261b1b8ff02ead0ec37d758df7d2f91
FG98 <81941690+FG98F@users.noreply.github.com>
2022-08-12 20:22:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('DyLux\nWhatsApp Bot', {
+say('DyLux -  FG98', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
