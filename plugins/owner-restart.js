081ce78c2adf808c9ee34f77d010fbc0b8ca00bc
FG98 <81941690+FG98F@users.noreply.github.com>
2022-08-16 11:28:52 -0400

    Update owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -1,7 +1,7 @@
 import { spawn } from 'child_process'
 let handler = async (m, { conn, isROwner, text }) => {
     if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
-    if (global.conn.user.jid == conn.user.jid) {
+    if (conn.user.jid == conn.user.jid) {
     await m.reply('🔄 Reiniciando Bot...\n Espere un momento')
     process.send('reset')
   } else throw 'eh'
