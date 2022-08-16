d67c6a02afcf8097044ff48b833b14fcde7303ec
FG98 <81941690+FG98F@users.noreply.github.com>
2022-08-16 10:36:06 -0400

    Update owner-addprem.js

--- plugins/owner-addprem.js
+++ plugins/owner-addprem.js
@@ -1,32 +1,25 @@
 import db from '../lib/database.js'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    let who
+let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
     else who = m.chat
     let user = db.data.users[who]
-    if (!who) throw `✳️ Etiqueta o menciona a alguien\n\n📌 Ejemplo : ${usedPrefix + command} @user 7`
-    let txt = text.replace('@' + who.split`@`[0], '').trim()
-    if (!txt) throw `✳️ Ingrese el número de días que tendrá premium`
-    if (isNaN(txt)) return m.reply(`✳️ Ingrese un número\n\nEjemplo : ${usedPrefix + command} @user 7`)
-    var jumlahHari = 86400000 * txt
-    var now = new Date() * 1
-    if (now < user.premiumTime) user.premiumTime += jumlahHari
-    else user.premiumTime = now + jumlahHari
-user.premium = true
-    
+    if (!who) throw `✳️ Etiqueta o menciona a alguien\n\n📌 Ejemplo : ${usedPrefix + command} @user`
+if (global.prems.includes(who.split`@`[0])) throw '✳️ El usuario Mensionado Ya es premium'
+global.prems.push(`${who.split`@`[0]}`)
+
 conn.reply(m.chat, `
 ✅ PREMIUM
 
 @${who.split`@`[0]} ahora te conviertes en un usuario premium
 ┌───────────
 ▢ *Nombre:* ${user.name}
-▢ *Expira en :* ${txt} Días
 └───────────
 `, m, { mentions: [who] })
 
 }
-handler.help = ['addprem <@tag> <días>']
+handler.help = ['addprem <@tag>']
 handler.tags = ['owner']
 handler.command = ['addprem', 'addpremium'] 
 
