86db19bba270f835edbaafbc635346823b3dd814
FG98 <81941690+FG98F@users.noreply.github.com>
2022-08-16 11:25:47 -0400

    Update owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,6 +1,8 @@
+import db from '../lib/database.js'
+
 import { execSync } from 'child_process'
 let handler = async (m, { conn, text }) => {
-if (global.conn.user.jid == conn.user.jid) {
+if (conn.user.jid == conn.user.jid) {
 let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
 //require('fs').readdirSync('plugins').map(v=>global.reload('', v))
 conn.reply(m.chat, stdout.toString(), m)
