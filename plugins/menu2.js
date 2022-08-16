c734d2fc7049f3d5909daf7c86f669e812bea7f0
FG98 <81941690+FG98F@users.noreply.github.com>
2022-08-16 11:54:56 -0400

    Update menu2.js

--- plugins/menu2.js
+++ plugins/menu2.js
@@ -18,12 +18,11 @@ envíame por t.me/fgsupp_bot el *audio + comando* con en el que responderá
 `
 //const pp = await (await fetch('https://i.ibb.co/qMG1JPY/fg.jpg')).buffer()
     let pp = './src/fg_logo.jpg' 
-    
-    conn.sendHydrated(m.chat, m2, '▢ DyLux  ┃ ᴮᴼᵀ\n▢ Sígueme en Instagram\nhttps://www.instagram.com/fg98._\n', pp, 'https://youtube.com/fg98f', 'YouTube', null, null, [
-     ['⏍ Info', '/botinfo'],
-      ['⌬ Grupos', '/gpdylux']
-    ], m)
-    
+    conn.sendButton(m.chat, m2, '▢ DyLux  ┃ ᴮᴼᵀ\n▢ Sígueme en Instagram\nhttps://www.instagram.com/fg98._\n', pp, [
+      ['⏍ Info', `${usedPrefix}botinfo`],
+      ['⌬ Grupos', `${usedPrefix}gpdylux`]
+    ],m)
+   
 }
 
 handler.help = ['menu2']
