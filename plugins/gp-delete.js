069a1b3841e3fae5629bb1b7c9b062ceeefb2fd0
FG98 <81941690+FG98F@users.noreply.github.com>
2022-08-21 14:07:46 -0400

    Update and rename tool-delete.js to gp-delete.js

--- plugins/gp-delete.js
+++ plugins/gp-delete.js
@@ -2,11 +2,11 @@ let handler = async (m, { conn }) => {
     if (!m.quoted) throw false
     let { chat, fromMe, isBaileys } = m.quoted
     if (!fromMe) throw false
-    if (!isBaileys) throw '✳️ *Ese no es mi mensaje*'
+   // if (!isBaileys) throw '✳️ *Ese no es mi mensaje*'
     conn.sendMessage(chat, { delete: m.quoted.vM.key })
 }
 handler.help = ['delete']
-handler.tags = ['main']
+handler.tags = ['group']
 handler.command = ['del', 'delete'] 
 handler.admin = true 
 
