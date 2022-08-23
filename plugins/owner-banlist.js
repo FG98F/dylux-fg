3172e23ccf9db5f232688896dffb70c11ca6e3d1
FG98 <81941690+FG98F@users.noreply.github.com>
2022-08-23 16:48:41 -0400

    Create owner-banlist.js

---
+++ plugins/owner-banlist.js
@@ -0,0 +1,21 @@
+import db from '../lib/database.js'
+
+let handler = async (m, { conn, usedPrefix }) => {
+    let chats = Object.entries(db.data.chats).filter(chat => chat[1].isBanned)
+    let users = Object.entries(db.data.users).filter(user => user[1].banned)
+    
+    m.reply(`
+*Lista de usuarios Baneados*
+▢ Total : ${users.length} Usuarios${users ? '\n' + users.map(([jid], i) => `
+
+${i + 1}. ${conn.getName(jid) == undefined ? 'Desconocido' : conn.getName(jid)}
+▢ ${jid}
+`.trim()).join('\n') : ''}
+`.trim())
+}
+handler.help = ['listban']
+handler.tags = ['owner']
+handler.command = ['banlist', 'listban'] 
+handler.owner = true
+
+export default handler
