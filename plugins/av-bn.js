3753f2df186bf04b92c435e203366a45b7337cd7
FG98 <81941690+FG98F@users.noreply.github.com>
2022-08-25 13:08:34 -0400

    Update av-bn.js

--- plugins/av-bn.js
+++ plugins/av-bn.js
@@ -1,16 +1,9 @@
-import util from 'util'
-import path from 'path'
 
 let handler = async (m, { conn}) => {
 
-let name = conn.getName(m.sender)
-
 let av = './src/mp3/4.mp3'
+conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 
-conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, {
-type: 'audioMessage', 
-ptt: true
-})
 }
 handler.customPrefix = /^(buenas noches)$/i
 handler.command = new RegExp
