67e94fa0971396ea8591ef36147820acfa059eda
FG98 <81941690+FG98F@users.noreply.github.com>
2022-08-29 21:42:44 -0400

    Update gp-delete.js

--- plugins/gp-delete.js
+++ plugins/gp-delete.js
@@ -1,13 +1,28 @@
-let handler = async (m, { conn }) => { 
-    if (!m.quoted) throw false
-    let { chat, fromMe, isBaileys } = m.quoted
-    if (!fromMe) throw false
-   // if (!isBaileys) throw '✳️ *Ese no es mi mensaje*'
-    conn.sendMessage(chat, { delete: m.quoted.vM.key })
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+	
+if (!m.quoted) throw `✳️ Responde al mensaje que desea eliminar`
+try {
+let key = {}
+ try {
+ 	key.remoteJid = m.quoted ? m.quoted.fakeObj.key.remoteJid : m.key.remoteJid
+	key.fromMe = m.quoted ? m.quoted.fakeObj.key.fromMe : m.key.fromMe
+	key.id = m.quoted ? m.quoted.fakeObj.key.id : m.key.id
+ 	key.participant = m.quoted ? m.quoted.fakeObj.participant : m.key.participant
+ } catch (e) {
+ 	console.error(e)
+ }
+ return conn.sendMessage(m.chat, { delete: key })
+ } catch {
+    return conn.sendMessage(m.chat, { delete: m.quoted.vM.key })
+}
 }
 handler.help = ['delete']
 handler.tags = ['group']
-handler.command = ['del', 'delete'] 
-handler.admin = true 
+handler.command = /^del(ete)?$/i
+
+handler.group = false
+handler.admin = true
+handler.botAdmin = true
 
 export default handler
