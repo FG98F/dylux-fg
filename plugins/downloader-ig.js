76d2aa524b09e9e057533d448cceb35f5028ee68
FG98 <81941690+FG98F@users.noreply.github.com>
2022-08-30 19:25:11 -0400

    fix  Instagram  downloader

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -3,6 +3,7 @@ import instagramGetUrl from 'fg-ig'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `✳️ Uso del comamdo\n *${usedPrefix + command}* https://www.instagram.com/p/CYHeKxyMj-J/?igshid=YmMyMTA2M2Y=`
+    m.react(rwait) 
     let results = (await instagramGetUrl(args[0])).url_list[0]
     conn.sendFile(m.chat, results, 'instagram.mp4', `✅ Resultado`, m)
     m.react(done)
