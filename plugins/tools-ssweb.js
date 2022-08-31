3e3e49edb075f629578519950eb0cdd8e9f2d7b2
FG98 <81941690+FG98F@users.noreply.github.com>
2022-08-31 11:15:48 -0400

    Update tools-ssweb.js

--- plugins/tools-ssweb.js
+++ plugins/tools-ssweb.js
@@ -1,13 +1,12 @@
 
-import fetch from 'node-fetch'
-import fg from 'fg-apis'
-
 let handler = async (m, { conn, command, args, text }) => {
-if (!args[0]) return m.reply('✳️ Ingrese la Url de una página')
+      
+    let full = /f$/i.test(command) ? 'on' : 'off'
+	
+    if (!args[0]) return m.reply('✳️ Ingrese la Url de una página')
     m.react(rwait) 
-    let ssweb = await fg.tools.ssweb(text)
-    let ss = ssweb.result
-	//let ss = await (await fetch(`https://shot.screenshotapi.net/screenshot?url=${args[0]}&full_page=true&fresh=true&output=image&file_type=png&wait_for_event=load`)).buffer() 
+	let url = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
+    let ss = 'http://hadi-api.herokuapp.com/api/ssweb?url=' + url + '&device=desktop&full=' + full
     conn.sendFile(m.chat, ss, 'ssweb.png', '✅ Captura de la Pagina', m)
    
    m.react(done) 
