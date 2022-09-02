7891353de68ca9fa67c0c220be88a809972ec78c
FG98 <81941690+FG98F@users.noreply.github.com>
2022-09-02 18:05:14 -0400

    Update and rename games-_tictactoe.js to games_tictactoe.js

--- plugins/games_tictactoe.js
+++ plugins/games_tictactoe.js
@@ -15,7 +15,7 @@ export async function before(m) {
     let room = Object.values(this.game).find(room => room.id && room.game && room.state && room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender) && room.state == 'PLAYING')
     if (room) {
         // m.reply(`[DEBUG]\n${parseInt(m.text)}`)
-        if (!/^([1-9]|(me)?nyerah|surrender)$/i.test(m.text))
+        if (!/^([1-9]|(me)?nyerah|surr?ender)$/i.test(m.text))
             return !0
         isSurrender = !/^[1-9]$/.test(m.text)
         if (m.sender !== room.game.currentTurn) { // nek wayahku
@@ -29,7 +29,7 @@ export async function before(m) {
             }))
         if (!isSurrender && 1 > (ok = room.game.turn(m.sender === room.game.playerO, parseInt(m.text) - 1))) {
             m.reply({
-                '-3': 'El juego ha terminado'',
+                '-3': 'El juego ha terminado',
                 '-2': 'Inválido',
                 '-1': 'Posición inválida',
                 0: 'Posición inválida',
@@ -61,22 +61,21 @@ export async function before(m) {
         }
         let winner = isSurrender ? room.game.currentTurn : room.game.winner
         let str = `
-${isWin ? `@${winner.split('@')[0]} Eres el ganador 🎉 *+${winScore} XP*` : isTie ? `Se acabó el juego, con un empate *+${playScore} XP*` : `Giro ${['❎', '⭕'][1 * room.game._currentTurn]} (@${room.game.currentTurn.split('@')[0]})`} 
+${isWin ? `@${winner.split('@')[0]} Eres el ganador 🎉 *+${winScore} XP*` : isTie ? `Se acabó el juego, con un empate *+${playScore} XP*` : `Ahora es tu turno ${['❎', '⭕'][1 * room.game._currentTurn]} (@${room.game.currentTurn.split('@')[0]})`} 
 
 ${arr.slice(0, 3).join('')}
 ${arr.slice(3, 6).join('')}
 ${arr.slice(6).join('')}
 
-▢ *JUGADOR 1* @${room.game.playerX.split('@')[0]} ❎
-▢ *JUGADOR 2* @${room.game.playerO.split('@')[0]} ⭕
-▢ *SALA ID*: ${room.id}
+▢ *JUGADOR 1* ❎ : @${room.game.playerX.split('@')[0]} 
+▢ *JUGADOR 2* ⭕ : @${room.game.playerO.split('@')[0]}
 
 Escriba *surrender* para darse por vencido 
 `.trim()
         let users = db.data.users
         if ((room.game._currentTurn ^ isSurrender ? room.x : room.o) !== m.chat)
             room[room.game._currentTurn ^ isSurrender ? 'x' : 'o'] = m.chat
-        const btn = isTie ? ['TicTacToe', '/ttt'] : ['Rendirse', 'surrender']
+        const btn = isTie ? ['TicTacToe', '/ttt'] : ['Surrender', 'surrender']
         if (room.x !== room.o)
             await this.sendButton(room.x, str, igfg, btn, m, {
                 mentions: this.parseMention(str)
