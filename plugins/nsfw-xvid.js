7910c6631ec447526c42ec0f529bc95d2033b655
FG98 <81941690+FG98F@users.noreply.github.com>
2022-09-01 21:38:08 -0400

    Create nsfw-xvid.js

---
+++ plugins/nsfw-xvid.js
@@ -0,0 +1,23 @@
+
+import axios from 'axios'
+import db from '../lib/database.js'
+
+let handler = async(m, { conn, usedPrefix, command }) => {
+	
+	if (!db.data.chats[m.chat].nsfw && m.isGroup) throw `❗El grupo no admite contenido nsfw \n\n Para habilitar escriba \n*${usedPrefix}enable* nsfw`
+	let vid = (await axios.get(`https://raw.githubusercontent.com/FG98F/team-fg/main/nsfw/xvid.json`)).data
+    conn.sendMessage(m.chat, { document: { url: pickRandom(vid)}, mimetype: 'video/mp4', fileName: 'xvid random.mp4'}, {quoted: m})
+    m.react(xmoji)
+}
+
+handler.help = ['xvid']
+handler.tags = ['nsfw', 'premium']
+handler.command = ['xvid', 'xvideos', 'xvideo']
+handler.premium = true
+handler.limit = true
+
+export default handler
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
