db8194be84a6c61ddd1880d394096d5391981c9a
FG98 <81941690+FG98F@users.noreply.github.com>
2022-09-03 18:46:16 -0400

    Update av-bot.js

--- plugins/av-bot.js
+++ plugins/av-bot.js
@@ -1,24 +1,17 @@
-import util from 'util'
-import path from 'path'
 
 let handler = async (m, { conn}) => {
 
 let name = conn.getName(m.sender)
-
-let fgac = ["criss", "andrea"]
-let nfg = pickRandom(fgac)
-let av = `./src/mp3/${nfg}.mp3`
+let av = `./src/mp3/${pickRandom(["criss", "andrea"])}.mp3`
 
 conn.sendButton(m.chat, `Hola *${name}* \n \nNecesitas ayuda? \n`, igfg, null, [
       ['⦙☰ Menu', '/help'],
       ['⦙☰ Menu 2', '/menu2'],
       ['⌬ Grupos', '/gpdylux']
     ], m)
-conn.sendFile(m.chat, av, 'ad.mp3', null, m, true, {
-type: 'audioMessage', 
-ptt: true
-})
-}
+conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+} 
+
 handler.customPrefix = /^(bot|dylux)$/i
 handler.command = new RegExp
 
