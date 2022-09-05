a031a54d13c1fd4440be646ea52c663afd271960
FG98 <81941690+FG98F@users.noreply.github.com>
2022-09-05 15:24:29 -0400

    Update _jsjs.js

--- plugins/_jsjs.js
+++ plugins/_jsjs.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-	let img = (await axios.get(`https://github.com/FG98F/team-fg/blob/main/img/hu.json`)).data
+	let img = (await axios.get(`https://raw.githubusercontent.com/FG98F/team-fg/main/img/hu.json`)).data
     let stiker = await sticker(null, global.API(`${pickRandom(img)}`), global.packname, global.author)
     if (stiker) return await conn.sendFile(m.chat, stiker, 'sticker.webp', { asSticker: true }, m)
     throw stiker.toString()   
