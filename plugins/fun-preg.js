8986c0711c6babcf02ce4490dc8719329e9fa52c
FG98 <81941690+FG98F@users.noreply.github.com>
2022-09-06 23:59:45 -0400

    Update fun-preg.js

--- plugins/fun-preg.js
+++ plugins/fun-preg.js
@@ -5,7 +5,8 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
  let name = conn.getName(m.sender)
   if (!text) throw `✳️ *Ejemplo :*\n\n *${usedPrefix + command}* soy feo?`
   m.react('🫣') 
-  let res = await fetch(global.API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(text), lc: "es" }, ''))
+  //let res = await fetch(global.API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(text), lc: "es" }, ''))
+  let res = await fetch(`https://api.simsimi.net/v2/?text=${text}&lc=es`)
   let json = await res.json()
   if (json.success) 
 m.reply(`≡ *PREGUNTAS*
