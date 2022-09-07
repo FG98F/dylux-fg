d42d1c150acd5a59cf2fcdab267cba8abf7e5849
FG98 <81941690+FG98F@users.noreply.github.com>
2022-09-06 22:11:06 -0400

    Update img-girl.js

--- plugins/img-girl.js
+++ plugins/img-girl.js
@@ -1,12 +1,13 @@
 
+import fetch from 'node-fetch'
 import axios from 'axios'
 
 let handler = async(m, { conn, usedPrefix, command }) => {
 	
-	let girl = (await axios.get(`https://raw.githubusercontent.com/FG98F/team-fg/main/img/girl.json`)).data
-  
-//await conn.sendFile(m.chat, pickRandom(girl), 'girl.jpg', ✅ Resultado 🤭', m)
-await conn.sendButton(m.chat, '✅ Resultado 🤭', 'Click en siguiente para ir a la siguiente imagen', pickRandom(girl), [['▷▷ SIGUIENTE', `${usedPrefix + command}`]],m)
+	//let girl = (await axios.get(`https://raw.githubusercontent.com/FG98F/team-fg/main/img/girl.json`)).data
+	let girl = await conn.getFile(`https://fg-dylux.herokuapp.com/api/img/girl?apikey=FG98`)
+	let img = girl.data
+   await conn.sendButton(m.chat, '✅ Resultado 🤭', 'Click en siguiente para ir a la siguiente imagen', img, [['▷▷ SIGUIENTE', `${usedPrefix + command}`]],m)
 }
 handler.help = ['girl']
 handler.tags = ['img']
