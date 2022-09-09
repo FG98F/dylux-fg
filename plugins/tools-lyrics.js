496fe8d94d2e8e07ba0db2b583c0ba1f9b69268f
FG98 <81941690+FG98F@users.noreply.github.com>
2022-09-09 14:18:29 -0400

    Update tools-lyrics.js

--- plugins/tools-lyrics.js
+++ plugins/tools-lyrics.js
@@ -3,6 +3,7 @@ import fetch from 'node-fetch'
 let handler = async (m, {conn, text }) => {
   let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : ''
    if (!teks) throw `✳️ Ingrese el nombre de la canción`
+   try {
   let res = await fetch(global.API('https://some-random-api.ml', '/lyrics', { title: teks }))
   if (!res.ok) throw await res.text()
   let json = await res.json()
@@ -11,6 +12,10 @@ let handler = async (m, {conn, text }) => {
 ▢ *${json.title}*
 *${json.author}*\n
 ${json.lyrics}`, m)
+m.react(done)
+} catch (e) {
+	m.react(error)
+	} 
 }
 handler.help = ['lyrics']
 handler.tags = ['tools']
