dd43ee5fa79b404bebe8ecbec23fbcb1cba831a5
FG98 <81941690+FG98F@users.noreply.github.com>
2022-09-09 07:52:34 -0400

    Update _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -3,6 +3,7 @@ import fetch from 'node-fetch'
 
  export async function all(m, { conn, groupMetadata }) {
 	
+	//reply link
     global.rpl = { contextInfo: { externalAdReply: {title: packname, body: 'support group' , sourceUrl: dygp, thumbnail: await(await fetch(fglog)).buffer() }}}
 	
     
