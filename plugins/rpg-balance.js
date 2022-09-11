fe225e971938e5a78947ea16b9d4ebb70ad2468f
FG98 <81941690+FG98F@users.noreply.github.com>
2022-09-11 13:06:54 -0400

    Update rpg-balance.js

--- plugins/rpg-balance.js
+++ plugins/rpg-balance.js
@@ -1,22 +1,29 @@
 //import db from '../lib/database.js'
+import { canLevelUp, xpRange } from '../lib/levelling.js'
 
 let handler = async (m, {conn, usedPrefix}) => {
 	
     let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
     else who = m.sender
-  let name = conn.getName(who) 
-    m.reply(`
+    let name = conn.getName(who) 
+    let user = global.db.data.users[who]
+   let { min, xp, max } = xpRange(user.level, global.multiplier)
+ 
+    conn.reply(m.chat, `
 ┌───⊷ *BALANCE* ⊶
-▢ *Nombre* : ${name}
-▢ *Diamantes* : *${global.db.data.users[who].diamond}💎*
+▢ *📌Nombre* : _@${who.split('@')[0]}_
+▢ *💎Diamantes* : _${global.db.data.users[who].diamond}_
+▢ *⬆️XP* : _${user.exp - min}_
 └──────────────
+
 *NOTA :* 
 Puedes comprar 💎 diamantes usando los comandos
 ❏ *${usedPrefix}buy <cantidad>*
-❏ *${usedPrefix}buyall*`)
+❏ *${usedPrefix}buyall*`, m, { mentions: [who] })
 }
 handler.help = ['bal']
 handler.tags = ['xp']
 handler.command = ['bal', 'diamantes', 'diamond', 'balance'] 
+
 export default handler
