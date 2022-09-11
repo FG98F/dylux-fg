c16f89e92ece31d2b8b11734953f6ef6629cb03f
FG98 <81941690+FG98F@users.noreply.github.com>
2022-09-11 13:26:38 -0400

    Update gp-profile.js

--- plugins/gp-profile.js
+++ plugins/gp-profile.js
@@ -1,21 +1,22 @@
 import { createHash } from 'crypto'
 import PhoneNumber from 'awesome-phonenumber'
+import { canLevelUp, xpRange } from '../lib/levelling.js'
 //import db from '../lib/database.js'
 
-
 let handler = async (m, { conn, usedPrefix, command}) => {
-
 
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 if (!(who in global.db.data.users)) throw `✳️ El usuario no se encuentra en mi base de datos`
 let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png')
+let user = global.db.data.users[who]
 let { name, exp, diamond, lastclaim, registered, regTime, age, level, role } = global.db.data.users[who]
+let { min, xp, max } = xpRange(user.level, global.multiplier)
 let username = conn.getName(who)
+let math = max - xp
 let prem = global.prems.includes(who.split`@`[0])
 let sn = createHash('md5').update(who).digest('hex')
-let user = global.db.data.users[who]
-user.role = global.rpg.role(user.level).name
 
+user.role = global.rpg.role(user.level).name 
 let str = `
 ┌───「 *PERFIL* 」
 ▢ *🔖 Nombres:* 
@@ -25,16 +26,16 @@ let str = `
 ▢ *🔗Link:* wa.me/${who.split`@`[0]}${registered ? '\n▢ *🎈Edad*: ' + age + ' años' : ''}
 ▢ *💎 Diamantes :* ${diamond}
 ▢ *🆙 Nivel* : ${level}
+▢ *⬆️ XP* : Total ${exp} (${user.exp - min} / ${xp})\n${math <= 0 ? `listo para *${usedPrefix}levelup*` : `_*${math}xp*_ Falta para subir de nivel`}
 ▢ *🏆Rango:* ${user.role}
 ▢ *📇 Registrado :* ${registered ? 'Si': 'No'}
 ▢ *⭐ Premium* : ${prem ? 'Si' : 'No'}
 └──────────────`
-
     conn.sendFile(m.chat, pp, 'perfil.jpg', str, m, false, { mentions: [who] })
     m.react(done)
 
 }
-handler.help = ['perfil @user']
+handler.help = ['perfil']
 handler.tags = ['group']
 handler.command = ['profile', 'perfil'] 
 
