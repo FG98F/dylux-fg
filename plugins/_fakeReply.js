840c600dceb5e8b3adf56e2ee0510648265864f8
FG98 <81941690+FG98F@users.noreply.github.com>
2022-09-11 10:13:21 -0400

    Update _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -1,7 +1,8 @@
 
 import fetch from 'node-fetch'
 
- export async function all(m, { conn, groupMetadata }) {
+let handler = m => m
+handler.all = async function (m) {
 	
 	let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender
 	let pp = await this.profilePictureUrl(who, 'image').catch(_ => fglog)
@@ -13,3 +14,4 @@ import fetch from 'node-fetch'
     global.rpyt = { contextInfo: { externalAdReply: { showAdAttribution: true, mediaUrl: fgyt, mediaType: 'VIDEO', description: 'Suscribete : ' + fgyt, title: 'FG YouTube', body: 'aprende a crear tus propios bots', thumbnailUrl: pp, sourceUrl: fgyt }}}
 
 } 
+export default handler
