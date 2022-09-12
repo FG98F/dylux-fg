741fd457f99cdf3a8f82e1dfcf56a26702550b01
FG98 <81941690+FG98F@users.noreply.github.com>
2022-09-12 13:35:34 -0400

    Update sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -5,8 +5,10 @@ import { webp2png } from '../lib/webp2mp4.js'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   let stiker = false
-  try {
-  	
+       let stick = args.join(" ").split("|");
+       let f = stick[0] !== "" ? stick[0] : packname;
+       let g = typeof stick[1] !== "undefined" ? stick[1] : author;
+  try { 	
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || q.mediaType || ''
     if (/webp|image|video/g.test(mime)) {
@@ -15,7 +17,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
       if (!img) throw `✳️ Responde a una imagen o video con*${usedPrefix + command}*`
       let out
       try {
-        stiker = await sticker(img, false, global.packname, global.author)
+        stiker = await sticker(img, false, f, g)
       } catch (e) {
         console.error(e)
       } finally {
@@ -24,7 +26,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
           else if (/image/g.test(mime)) out = await uploadImage(img)
           else if (/video/g.test(mime)) out = await uploadFile(img)
           if (typeof out !== 'string') out = await uploadImage(img)
-          stiker = await sticker(false, out, global.packname, global.author)
+          stiker = await sticker(false, out, f, g)
         }
       }
     } else if (args[0]) {
