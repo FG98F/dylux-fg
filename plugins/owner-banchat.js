da973935cb9a7d40288504e8613871b90b7c32ea
FG98 <81941690+FG98F@users.noreply.github.com>
2022-09-13 09:40:17 -0400

    Update owner-banchat.js

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -1,15 +1,13 @@
 //import db from '../lib/database.js'
 
 let handler = async (m, { conn, participants }) => {
-    // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
     global.db.data.chats[m.chat].isBanned = true
     m.reply('✅ Se desactivo el Bot en este grupo')
-    // } else m.reply('Aquí hay un número de host...')
 }
 handler.help = ['banchat']
 handler.tags = ['owner']
 handler.command = ['banchat', 'chatoff'] 
-
-handler.owner = true
+handler.owner = false
+handler.admin = true
 
 export default handler
