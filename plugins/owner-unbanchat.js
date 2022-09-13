71bb4be31a02fedadd5c284eafbe2a3af7757964
FG98 <81941690+FG98F@users.noreply.github.com>
2022-09-13 18:54:24 -0400

    Update owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,6 +1,6 @@
 //import db from '../lib/database.js'
 
-let handler = async (m, { isOwner, isAdmin, isROwner} ) => {
+let handler = async (m, { conn, isOwner, isAdmin, isROwner} ) => {
     if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = false
     m.reply('✅ Bot activo  en este grupo')   
