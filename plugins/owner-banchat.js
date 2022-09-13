d1ee56c5b38c4de72290c386f41cfda130314fd2
FG98 <81941690+FG98F@users.noreply.github.com>
2022-09-13 18:52:07 -0400

    Add files via upload

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -1,12 +1,12 @@
 //import db from '../lib/database.js'
 
 let handler = async (m, { conn, participants }) => {
+	if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = true
     m.reply('✅ Se desactivo el Bot en este grupo')
 }
 handler.help = ['banchat']
 handler.tags = ['owner']
 handler.command = ['banchat', 'chatoff'] 
-handler.owner, handler.admin = true
 
 export default handler
--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,12 +1,12 @@
 //import db from '../lib/database.js'
 
-let handler = async (m) => {
+let handler = async (m, { isOwner, isAdmin, isROwner} ) => {
+    if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = false
-    m.reply('✅ Bot activo  en este grupo')
+    m.reply('✅ Bot activo  en este grupo')   
 }
 handler.help = ['unbanchat']
 handler.tags = ['owner']
 handler.command = ['chaton', 'unbanchat'] 
-handler.owner, handler.admin = true
 
 export default handler
