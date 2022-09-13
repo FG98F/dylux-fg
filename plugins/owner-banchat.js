558b30743abf964e88ecd8b0adc68d6f11ad757c
FG98 <81941690+FG98F@users.noreply.github.com>
2022-09-13 18:31:14 -0400

    Update owner-banchat.js

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, participants }) => {
 handler.help = ['banchat']
 handler.tags = ['owner']
 handler.command = ['banchat', 'chatoff'] 
-handler.owner = false
+handler.owner = true
 handler.admin = true
 
 export default handler
