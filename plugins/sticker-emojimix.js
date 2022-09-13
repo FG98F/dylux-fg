fed729b17b1cab101d7934fbbe9cfbd947c9c475
FG98 <81941690+FG98F@users.noreply.github.com>
2022-09-13 10:26:26 -0400

    Update sticker-emojimix.js

--- plugins/sticker-emojimix.js
+++ plugins/sticker-emojimix.js
@@ -15,6 +15,7 @@ reject(err)
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 	
 if (!args[0]) throw `📌 Ejemplo : ${usedPrefix + command} 😎+🤑`
+if (!text.includes('+')) throw  `✳️ Separe el emoji con un *+* \n\n📌 Ejemplo : \n*${usedPrefix + command}* 😎+🤑`
 let [emoji, emoji2] = text.split`+`
 let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji)}_${encodeURIComponent(emoji2)}`)
 for (let res of anu.results) {
