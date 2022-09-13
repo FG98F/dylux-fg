072dc7400cb6e559ea4eb687f7608d8f77164b85
FG98 <81941690+FG98F@users.noreply.github.com>
2022-09-13 07:43:33 -0400

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ import { platform } from 'process'
 global.__filename = function filename(pathURL = import.meta.url, rmPrefix = platform !== 'win32') { return rmPrefix ? /file:\/\/\//.test(pathURL) ? fileURLToPath(pathURL) : pathURL : pathToFileURL(pathURL).toString() }; global.__dirname = function dirname(pathURL) { return path.dirname(global.__filename(pathURL, true)) }; global.__require = function require(dir = import.meta.url) { return createRequire(dir) }
 
 import * as ws from 'ws';
-import { 
+import {
   readdirSync,
   statSync,
   unlinkSync,
