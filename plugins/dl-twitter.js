d085955e3ce18f08766cb0bcb5059f3c6c12db51
FG98 <81941690+FG98F@users.noreply.github.com>
2022-09-13 20:59:44 -0400

    Create dl-twitter.js

---
+++ plugins/dl-twitter.js
@@ -0,0 +1,23 @@
+import twitterDl from 'fg-twitter'
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `✳️ Ejemplo : \n${usedPrefix + command} https://twitter.com/fernandavasro/status/1569741835555291139?t=ADxk8P3Z3prq8USIZUqXCg&s=19`
+    let res = await twitterDl(args[0])
+    try {
+    for (let result of res.download) {
+    	let res = result.url.replace('https://ssstwitter.com', '')
+       conn.sendFile(m.chat, res, 'twitter.mp4', '✅ Resultado' , m)
+  }
+  } catch (e) {
+  	m.reply('✳️ Revise el link sea de un video de Twitter')
+	m.react(error)
+	} 
+  
+}
+handler.help = ['twitter'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+handler.command = /^(twitter|tw)$/i
+
+handler.diamond = true
+
+export default handler
