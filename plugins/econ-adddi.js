62766972df68d64cdc14deceb61b0ebf4f15b332
FG98 <81941690+FG98F@users.noreply.github.com>
2022-09-16 00:04:27 -0400

    Update and rename rpg-adddi.js to econ-adddi.js

--- plugins/econ-adddi.js
+++ plugins/econ-adddi.js
@@ -26,7 +26,7 @@ let handler = async (m, { conn, text }) => {
 }
 
 handler.help = ['adddi <@user>']
-handler.tags = ['xp']
+handler.tags = ['econ']
 handler.command = ['adddi'] 
 handler.rowner = true
 
