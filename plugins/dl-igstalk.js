7fada8181db4301823c042dfe1ac58509f11a624
FG98 <81941690+FG98F@users.noreply.github.com>
2022-09-16 00:15:16 -0400

    Update dl-igstalk.js

--- plugins/dl-igstalk.js
+++ plugins/dl-igstalk.js
@@ -23,7 +23,7 @@ let handler= async (m, { conn, args, text, usedPrefix, command }) => {
 }
 
 handler.help = ['igstalk']
-handler.tags = ['downloader']
+handler.tags = ['dl']
 handler.command = ['igstalk'] 
 
 export default handler
