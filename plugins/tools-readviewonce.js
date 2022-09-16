140701f695113088a0e34377974aca1378bfb88d
FG98 <81941690+FG98F@users.noreply.github.com>
2022-09-16 00:19:36 -0400

    Update and rename tool-readviewonce.js to tools-readviewonce.js

--- plugins/tools-readviewonce.js
+++ plugins/tools-readviewonce.js
@@ -20,7 +20,6 @@ let handler = async (m, { conn }) => {
 
 handler.help = ['readvo']
 handler.tags = ['tools']
-
 handler.command = ['readviewonce', 'read', 'ver', 'readvo'] 
 
 export default handler
