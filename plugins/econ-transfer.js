b4c8d3184da253166f6d976f972c15a4a38ebb67
FG98 <81941690+FG98F@users.noreply.github.com>
2022-09-16 00:06:29 -0400

    Update and rename rpg-transfer.js to econ-transfer.js

--- plugins/econ-transfer.js
+++ plugins/econ-transfer.js
@@ -72,7 +72,7 @@ handler.before = async m => {
 }
 
 handler.help = ['transfer'].map(v => v + ' [tipo] [monto] [@tag]')
-handler.tags = ['xp']
+handler.tags = ['econ']
 handler.command = ['payxp','paydi', 'transfer', 'darxp','dardi',]
 
 handler.disabled = false
