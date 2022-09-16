3e5d8550b01b13da8ceaba05b4fcf9550a2acd74
FG98 <81941690+FG98F@users.noreply.github.com>
2022-09-16 00:30:56 -0400

    Update and rename group-add.js to gp-add.js

--- plugins/gp-add.js
+++ plugins/gp-add.js
@@ -43,15 +43,13 @@ let handler = async (m, { conn, text, participants }) => {
         await conn.sendGroupV4Invite(m.chat, jid, invite_code, invite_code_exp, await conn.getName(m.chat), 'Invitación para unirse a mi grupo de WhatsApp ', jpegThumbnail)
     }
 }
-handler.help = ['Add'].map(v => v + ' <Número>')
+handler.help = ['add'].map(v => v + ' <num>')
 handler.tags = ['group']
 handler.command = ['add']
-
 handler.admin = true
 handler.owner = true
 handler.group = true
 handler.botAdmin = true
-
 
 export default handler
 
