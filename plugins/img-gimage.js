0b21d5cdc0527886a9ff8b085c660b47d5ae7e5e
FG98 <81941690+FG98F@users.noreply.github.com>
2022-09-18 17:37:30 -0400

    Update img-gimage.js

--- plugins/img-gimage.js
+++ plugins/img-gimage.js
@@ -1,14 +1,29 @@
-import { googleImage } from '@bochilteam/scraper'
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw `✳️ Que imagen busco?`
-    const res = await googleImage(text)
-    conn.sendFile(m.chat, res.getRandom(), 'image.jpg', `
+
+import { promisify } from 'util'
+import _gis from 'g-i-s'
+let gis = promisify(_gis)
+
+let handler  = async (m, { conn, args, text, usedPrefix, command }) => {
+  if (!text) throw `✳️ Ingrese la imagen que quiere buscar \n\n📌 Ejemplo: *${usedPrefix + command}* Billie Eilish`
+  let results = await gis(text) || []
+  let { url, width, height } = pickRandom(results) || {}
+  if (!url) throw '❎ No se encontró la imagen intente con otro'
+  conn.sendFile(m.chat, url, 'img.png', `
 ✅ Resultado de : *${text}*
+
+⏣ *Ancho*: ${width}
+⏣ *Altura*: ${height}
 `.trim(), m)
 }
 handler.help = ['imagen']
 handler.tags = ['img']
-handler.command = ['img', 'image', 'imagen'] 
+handler.command = /^(img|image|gimage|imagen)$/i
 handler.diamond = true
 
 export default handler
+
+function pickRandom(arr) {
+  return arr[Math.floor(Math.random() * arr.length)]
+}
+
+
