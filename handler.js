adeb0d6a4729f0413b27597ed960b8e66fcecee7
FG98 <81941690+FG98F@users.noreply.github.com>
2022-09-17 20:36:28 -0400

    Update handler.js

--- handler.js
+++ handler.js
@@ -549,16 +549,16 @@ Para desactivar esta función, escriba
 
 global.dfail = (type, m, conn) => {
     let msg = {
-        rowner: '✳️ Este comando solo puede ser utilizado por el *Creador del bot*',
-        owner: '✳️ Este comando solo puede ser utilizado por el *Dueño del Bot*',
-        mods: '✳️  Esta función es solo para *Para moderadores del Bot*',
-        premium: '✳️ Este comando es solo para miembros *Premium*',
-        group: '✳️ ¡Este comando solo se puede usar en grupos!',
-        private: '✳️ Este comando solo se puede usar en el chat *privado del Bot*',
-        admin: '✳️ Este comando es solo para *Admins* del grupo',
-        botAdmin: '✳️ ¡Para usar este comando debo ser *Administrador!*',
-        unreg: 'Regístrese para usar esta función  Escribiendo:\n\n*/reg nombre.edad*\n\n📌Ejemplo : */reg dylux.16*',
-        restrict: '¡Esta característica está *deshabilitada*'
+        rowner: '👑 Este comando solo puede ser utilizado por el *Creador del bot*',
+        owner: '🔱 Este comando solo puede ser utilizado por el *Dueño del Bot*',
+        mods: '🔰  Esta función es solo para *Para moderadores del Bot*',
+        premium: '💠 Este comando es solo para miembros *Premium*\n\nEscribe */premium* para más info',
+        group: '⚙️ ¡Este comando solo se puede usar en grupos!',
+        private: '📮 Este comando solo se puede usar en el chat *privado del Bot*',
+        admin: '🛡️ Este comando es solo para *Admins* del grupo',
+        botAdmin: '💥 ¡Para usar este comando debo ser *Administrador!*',
+        unreg: '📇 Regístrese para usar esta función  Escribiendo:\n\n*/reg nombre.edad*\n\n📌Ejemplo : */reg dylux.16*',
+        restrict: '🔐 Esta característica está *deshabilitada*'
     }[type]
     if (msg) return conn.sendButton(m.chat, msg, igfg, null, [['🔖 OK', 'khajs'], ['⦙☰ Menu', '/menu'] ], m)
 }
