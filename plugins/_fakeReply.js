2aa011bfa01991ea4d7267ed6f456f900ea705c3
FG98 <81941690+FG98F@users.noreply.github.com>
2022-09-19 15:36:20 -0400

    Update _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -5,10 +5,10 @@ let handler = m => m
 handler.all = async function (m) {
 	
 	let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender
-	let pp = await this.profilePictureUrl(who, 'image').catch(_ => fglog)
+	let pp = await this.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png')
 	
 	//reply link wa
-    global.rpl = { contextInfo: { externalAdReply: { title: packname, body: 'support group' , sourceUrl: dygp, thumbnail: await(await fetch(fglog)).buffer() }}}
+    global.rpl = { contextInfo: { externalAdReply: { title: packname, body: 'support group' , sourceUrl: dygp, thumbnail: await(await fetch(pp)).buffer() }}}
 	
 	//reply link yt
     global.rpyt = { contextInfo: { externalAdReply: { showAdAttribution: true, mediaUrl: fgyt, mediaType: 'VIDEO', description: 'Suscribete : ' + fgyt, title: 'FG YouTube', body: 'aprende a crear tus propios bots', thumbnailUrl: pp, sourceUrl: fgyt }}}
