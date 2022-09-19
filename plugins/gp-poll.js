608c3740d759633af7687a630298724d2e7920e4
FG98 <81941690+FG98F@users.noreply.github.com>
2022-09-19 08:15:53 -0400

    Update gp-poll.js

--- plugins/gp-poll.js
+++ plugins/gp-poll.js
@@ -1,17 +1,19 @@
 
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 	
-if (!args[0]) throw `✳️ Falta texto para encuesta \n\n📌 Ejemplo : \n*${usedPrefix + command}* texto|texto2...`
-if (!text.includes('|')) throw  `✳️ Separe las encuestas con *|* \n\n📌 Ejemplo : \n*${usedPrefix + command}* texto|texto2...`
+if (!args[0]) throw `✳️ Falta texto para encuesta \n\n📌 Ejemplo : \n*${usedPrefix + command}* Hola|como|xd`
+if (!text.split('|')[1]) throw  `✳️ Separe las encuestas con *|* \n\n📌 Ejemplo : \n*${usedPrefix + command}* Hola|como|xd`
+
+let name = await conn.getName(m.sender)
 let a = []
 let b = text.split('|')
-for (let c = 0; c < b.length; c++) {
+for (let c = 1 || 0; c < b.length; c++) {
 a.push([b[c]])
 			}
-			return conn.sendPoll(m.chat, `📊 *Encuesta para:*\n\n${text}`, a, m)
+			return conn.sendPoll(m.chat, `📊 *Encuesta solicitado por:* ${name}\n\n*Mensaje:* ${text.split('|')[0]}`, a, m)
 }
-handler.help = ['poll <text|text2>']
+handler.help = ['poll <hola|como|xd>']
 handler.tags = ['group'] 
-handler.command = ['poll', 'encuesta'] 
+handler.command = ['poll', 'encuesta', 'polling'] 
 
 export default handler
