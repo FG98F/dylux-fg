c9e004da9f032810ac4a2fc4722f285b9e147dd7
FG98 <81941690+FG98F@users.noreply.github.com>
2022-09-20 16:27:49 -0400

    Update dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -1,26 +1,24 @@
-import { tiktokdl, tiktokdlv2, tiktokdlv3 } from '@bochilteam/scraper'
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
+import fg from 'fg-dylux'
+
+let handler = async (m, { conn, text, args, usedPrefix, command}) => {
 if (!args[0]) throw `✳️ Ingrese un link de Tiktok\n\n 📌 Ejemplo : ${usedPrefix + command} https://vm.tiktok.com/ZMNqyusVD/?k=1`
 if (!args[0].match(/tiktok/gi)) throw `❎ verifica que el link sea de tiktok`
-
-    const { author: { nickname }, video, description } = await tiktokdl(args[0])
-         .catch(async _ => await tiktokdlv2(args[0]))
-         .catch(async _ => await tiktokdlv3(args[0]))
-    //const url = video.no_watermark || video.no_watermark2 || video.no_watermark_raw
-   const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd
-    if (!url) throw '❎ Error al descargar el video'
-    m.react(rwait)
-    conn.sendFile(m.chat, url, 'tiktok.mp4', `
+m.react(rwait)
+    let p = await fg.downloader.tiktok(text) 
+				let te = `
 ┌─⊷ TIKTOK
-▢ *Nickname:* ${nickname} ${description ? `\n▢ *Descripción:* ${description}` : ''}
-└───────────
-`.trim(), m)
-
-m.react(done)
-}
+▢ *Creador:* FG98
+▢ *Username:* ${p.author}
+▢ *Descripción:* ${p.title}
+└───────────`
+    conn.sendButton(m.chat, te, igfg, p.nowm, [['⎘ Stalkig', `${usedPrefix}ttstalk ${p.author.replace(/^@/, '')}`], ['♫ Audio', `${usedPrefix}tomp3`]],m)
+    m.react(done)
+    
+}  
 handler.help = ['tiktok']
 handler.tags = ['dl']
-handler.command = ['tiktok', 'tiktoknowm', 'tiktokdl'] 
+handler.command = /^(tiktok|ttdl|tiktokdl|tiktoknowm)$/i
+handler.diamond = true
 
 export default handler
