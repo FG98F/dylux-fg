104458a93bce8e22f5815230a42d1fff142cbe49
FG98 <81941690+FG98F@users.noreply.github.com>
2022-09-24 23:09:01 -0400

    Update dl-instagram.js

--- plugins/dl-instagram.js
+++ plugins/dl-instagram.js
@@ -3,15 +3,15 @@ import instagramGetUrl from 'fg-ig'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `✳️ Uso del comamdo\n *${usedPrefix + command}* https://www.instagram.com/p/CYHeKxyMj-J/?igshid=YmMyMTA2M2Y=`
-    m.react(rwait) 
-    let results = (await instagramGetUrl(args[0])).url_list[0]
-    conn.sendFile(m.chat, results, 'instagram.mp4', `✅ Resultado`, m)
+    let res = await instagramGetUrl(args[0])
+    for (let result of res.url_list) {
+    conn.sendFile(m.chat, result, 'igdl.mp4', `✅ Resultado`, m)
     m.react(done)
+  }
 }
-
 handler.help = ['instagram <link ig>']
 handler.tags = ['dl']
 handler.command = ['ig', 'igdl', 'instagram', 'igimg', 'igvid'] 
 handler.diamond = true
 
-export default handler
+export default handler 
