d56f10931ad378aa6f260a3bbdb11f6159335069
FG98 <81941690+FG98F@users.noreply.github.com>
2022-09-24 20:58:17 -0400

    Update dl-ytv.js

--- plugins/dl-ytv.js
+++ plugins/dl-ytv.js
@@ -1,57 +1,39 @@
 
-//import db from '../lib/database.js'
+let limit = 350
 import fetch from 'node-fetch'
 import fg from 'fg-dylux' 
-import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper';
-let limit = 80
-let handler = async (m, { conn, args, text, isPrems, isOwner, usedPrefix, command }) => {
-  if (!args || !args[0]) throw `✳️ Ejemplo :\n${usedPrefix + command} https://youtu.be/YzkTFFwxtXI`
+import { ytv } from '../lib/y2mate.js'
+let handler = async (m, { conn, args, text, isPrems, isOwner }) => {
+   if (!args || !args[0]) throw `✳️ Ejemplo :\n${usedPrefix + command} https://youtu.be/YzkTFFwxtXI`
   if (!args[0].match(/youtu/gi)) throw `❎ Verifica que el link de YouTube`
- m.react(rwait)
  let chat = global.db.data.chats[m.chat]
+ m.react(rwait) 
  
  try {
-  const isY = /y(es)/gi.test(args[1])
- const { thumbnail, video: _video, title } = await youtubedl(args[0]).catch(async _ => await youtubedlv2(args[0])).catch(async _ => await youtubedlv3(args[0]))
-  const limitedSize = (isPrems || isOwner ? 350 : limit) * 3074
-  let video, source, res, link, lastError, isLimit
-  for (let i in _video) {
-    try {
-      video = _video[i]
-      if (isNaN(video.fileSize)) continue
-      isLimit = limitedSize < video.fileSize
-      if (isLimit) continue
-      link = await video.download()
-      if (link) res = await fetch(link)
-      isLimit = res?.headers.get('content-length') && parseInt(res.headers.get('content-length')) < limitedSize
-      if (isLimit) continue
-      if (res) source = await res.arrayBuffer()
-      if (source instanceof ArrayBuffer) break
-    } catch (e) {
-      video = source = link = null
-      lastError = e
-    }
-  }
-  if ((!(source instanceof ArrayBuffer) || !link || !res.ok) && !isLimit) throw '❎ Error: ' + (lastError || 'no puedo descargar el video')
-
-  let _thumb = {}
-  try { _thumb = { thumbnail: await (await fetch(thumbnail)).buffer() } }
-  catch (e) { }
-  if (!isLimit) await conn.sendFile(m.chat, link, title + '.mp4', `
+  let { title, size, link, thumb, tipe, quality } = await ytv(text)
+  if(Number(size.split(' MB')[0]) >= limit) {
+     axios.get(`https://tinyurl.com/api-create.php?url=${link}`).then((G) => {
+     return m.reply(`
+▢ *📌Título* : ${title}
+▢ *⚖️Peso* : ${size}
+▢ _El archivo supera el límite de descarga_ *+${limit}MB*
+▢ Descarga  Aquí ${G.data}` )  }) 
+      } else {  
+ conn.sendFile(m.chat, link, title + '.mp4', `
  ≡  *FG MUSIC*
   
 ▢ *📌Título* : ${title}
-▢ *📟 Ext* : mp4
-▢ *⚖️Peso* : ${video.fileSizeH}
-`.trim(), m, false, {
-    ..._thumb,
-    asDocument: chat.useDocument
-  })
-  m.react(done)
-  } catch {  	
+▢ *📟Tipo* : ${tipe}
+▢ *⚖️Peso* : ${size}
+▢ *🎞️Calidad* : ${quality}
+`.trim(), m, false, { asDocument: chat.useDocument })
+m.react(done)
+} 
+//-
+ } catch {  	
 	fg.downloader.youtube.ytmp4(text).then((res) => {
      conn.sendFile(m.chat, res.result, res.title + '.mp4', `
- ≡  *FG MUSIC*
+ ≡  *FG MUSIC 2*
   
 ▢ *📌Título* : ${res.title}
 ▢ *📟 Ext* : mp4
@@ -60,12 +42,12 @@ let handler = async (m, { conn, args, text, isPrems, isOwner, usedPrefix, comman
 `.trim(), m, false, { asDocument: chat.useDocument })
  m.react(done)
 }) 
-} 
-  
-  
+}  
+//---
+
 }
 handler.help = ['ytmp4 <link yt>']
-handler.tags = ['dl']
+handler.tags = ['dl'] 
 handler.command = ['ytmp4', 'fgmp4']
 handler.diamond = true
 
