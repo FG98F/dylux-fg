c0b81139a7ec0924b37b0ab047e2cde7b68a05e5
FG98 <81941690+FG98F@users.noreply.github.com>
2022-09-26 08:46:24 -0400

    Update econ-rob.js

--- plugins/econ-rob.js
+++ plugins/econ-rob.js
@@ -1,18 +1,15 @@
 
 let handler = async (m, { conn, usedPrefix, command}) => {
     let time = global.db.data.users[m.sender].lastrob + 7200000
-    if (new Date - global.db.data.users[m.sender].lastrob < 7200000) throw `⏱️ Espera *${msToTime(time - new Date())}* para volver a robar`
+    if (new Date - global.db.data.users[m.sender].lastrob < 7200000) throw `⏱️¡Hey! Espera *${msToTime(time - new Date())}* para volver a robar`
     let who
-      if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
-      else who = m.chat
-      if (!who) throw `✳️ Etiqueta a alguien para robar`
-      if (!(who in global.db.data.users)) throw `✳️ El usuario no se encuentra en mi base de datos`
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
+    else who = m.chat
+    if (!who) throw `✳️ Etiqueta a alguien para robar`
+    if (!(who in global.db.data.users)) throw `✳️ El usuario no se encuentra en mi base de datos`
     let users = global.db.data.users[who]
     let rob = Math.floor(Math.random() * 3000)
-    if (users.exp < rob) {
-      m.reply(`🔖 @${who.split`@`[0]} tiene menos de *${rob} xp*\nNo robes a un podre v":`, null, { mentions: [who] })
-    }
-       
+    if (users.exp < rob) return m.reply(`🔖 @${who.split`@`[0]} tiene menos de *${rob} xp*\nNo robes a un podre v":`, null, { mentions: [who] })    
    global.db.data.users[m.sender].exp += rob
    global.db.data.users[who].exp -= rob
   
