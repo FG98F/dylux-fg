24eb93df0eb027bb4f50130c6c95cdbb1d61d07c
FG98 <81941690+FG98F@users.noreply.github.com>
2022-09-30 17:45:57 -0400

    Update and rename maker.mloli.js to maker.logos.js

---
+++ plugins/maker.logos.js
@@ -0,0 +1,44 @@
+
+let handler = async (m, { conn, args, text, usedPrefix, command }) => {
+	
+   let tee = `✳️ Ingrese un texto corto\n\n📌 Ejemplo  : *${usedPrefix + command}* FG98`
+   let too = `✳️ Separe EL texto con un *+* \n\n📌 Ejemplo : \n*${usedPrefix + command}* fgmods *+* DyLux`
+    m.react(rwait)
+let type = (command).toLowerCase()
+switch (type) {
+	
+	case 'logololi':
+	if (!text) throw tee
+	let img = global.API('fgmods', '/api/lolimaker', { text }, 'apikey')
+	conn.sendFile(m.chat, img, 'logo.png', `✅ Resultado`, m)
+	m.react(done)
+	break 
+	case 'neon': 
+	if (!text) throw tee
+	let ne = global.API('fgmods', '/api/textpro/neon', { text }, 'apikey')
+	conn.sendFile(m.chat, ne, 'logo.png', `✅ Resultado`, m)
+	m.react(done)
+	break 
+	case 'devil': 
+	if (!text) throw tee
+	let de = global.API('fgmods', '/api/textpro/devil', { text }, 'apikey')
+	conn.sendFile(m.chat, de, 'logo.png', `✅ Resultado`, m)
+	m.react(done)
+	break 
+	case 'wolf': 
+   if (!text) throw too
+   if (!text.includes('+')) throw too
+   let [a, b] = text.split`+`
+	let wo = global.API('fgmods', '/api/textpro/logowolf2', { text: a, text2: b}, 'apikey')
+	conn.sendFile(m.chat, wo, 'logo.png', `✅ Resultado`, m)
+	m.react(done)
+	break 
+	default:
+} 
+} 
+handler.help = ['logololi', 'neon', 'devil', 'wolf']
+handler.tags = ['maker']
+handler.command = /^(logololi|neon|devil|wolf)$/i
+handler.diamond = true
+
+export default handler
