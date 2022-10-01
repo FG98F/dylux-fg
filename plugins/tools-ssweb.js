233de4c1d6a3320f6c776e097df6ca7af98bd5c5
FG98 <81941690+FG98F@users.noreply.github.com>
2022-10-01 18:52:08 -0400

    Update tools-ssweb.js

--- plugins/tools-ssweb.js
+++ plugins/tools-ssweb.js
@@ -1,22 +1,18 @@
 
+import fetch from 'node-fetch'
 let handler = async (m, { conn, command, args, text }) => {
       
-    let full = /f$/i.test(command) ? 'on' : 'off'
-	
     if (!args[0]) return m.reply('✳️ Ingrese la Url de una página')
     m.react(rwait) 
-	let url = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
-    let ss = 'http://hadi-api.herokuapp.com/api/ssweb?url=' + url + '&device=desktop&full=' + full
-    conn.sendFile(m.chat, ss, 'ssweb.png', '✅ Captura de la Pagina', m)
-   
+	let full = /f$/i.test(command)
+    let u = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
+    let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url: u }))).buffer()
+    conn.sendFile(m.chat, ss, 'ssweb.png', '✅ Captura de la Pagina', m) 
    m.react(done) 
-
 }
 handler.help = ['ssweb <url>']
 handler.tags = ['tools']
 handler.command = ['ssweb', 'ss', 'captura'] 
-
 handler.diamond = true
-handler.fail = null
 
 export default handler
