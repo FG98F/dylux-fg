61a217732e50500860aae3e74665497053bac2ef
FG98 <81941690+FG98F@users.noreply.github.com>
2022-10-01 08:43:02 -0400

    Update _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -5,13 +5,13 @@ let handler = m => m
 handler.all = async function (m) {
 	
 	let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender
-	let pp = await this.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png')
+	let pp = await this.profilePictureUrl(who, 'image').catch(_ => 'https://i.imgur.com/whjlJSf.jpg')
 	
 	//reply link wa
     global.rpl = { contextInfo: { externalAdReply: { title: packname, body: 'support group' , sourceUrl: dygp, thumbnail: await(await fetch(fglog)).buffer() }}}
 	
 	//reply link PayPal
-    //global.rpl = { contextInfo: { externalAdReply: { title: packname, body: 'support group' , sourceUrl: dygp, thumbnail: await(await fetch(fglog)).buffer() }}}
+    global.rpyp = { contextInfo: { externalAdReply: { title: 'PayPal', body: 'support group' , sourceUrl: fgpyp, thumbnail: pp }}}
 	
 	//reply link yt
     global.rpyt = { contextInfo: { externalAdReply: { showAdAttribution: true, mediaUrl: fgyt, mediaType: 'VIDEO', description: 'Suscribete : ' + fgyt, title: 'FG YouTube', body: 'aprende a crear tus propios bots', thumbnailUrl: pp, sourceUrl: fgyt }}}
