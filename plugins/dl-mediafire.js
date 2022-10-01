d942bc65055267417a386363b6f6b07bebc99897
FG98 <81941690+FG98F@users.noreply.github.com>
2022-10-01 18:57:41 -0400

    Update dl-mediafire.js

--- plugins/dl-mediafire.js
+++ plugins/dl-mediafire.js
@@ -1,10 +1,20 @@
+
+import fetch from 'node-fetch'
 import { mediafiredl } from '@bochilteam/scraper'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `✳️ Ingrese el link de mediafire junto al comando`
+
+let handler = async (m, { conn, args, usedPrefix, command, isOwner, isPrems }) => {
+	var limit
+     if((isOwner || isPrems)) limit = 1200
+     else limit = 100
+   if (!args[0]) throw `✳️ Ingrese el link de mediafire junto al comando`
     if (!args[0].match(/mediafire/gi)) throw `❎ Link incorrecto`
     m.react(rwait)
+    let full = /f$/i.test(command)
+    let u = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
+    let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url: u }))).buffer()
     let res = await mediafiredl(args[0])
     let { url, url2, filename, ext, aploud, filesize, filesizeH } = res
+    let isLimit = (isPrems || isOwner ? limit : limit) * 1012 < filesize
     let caption = `
    ≡ *MEDIAFIRE*
 
@@ -12,15 +22,17 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 ▢ *Tamaño:* ${filesizeH}
 ▢ *Extension:* ${ext}
 ▢ *Subido:* ${aploud}
+${isLimit ? `\n▢ El archivo supera el límite de descarga *+${limit} MB*\nPásate a premium para poder descargar archivos más de *900 MB*` : ''} 
 `.trim()
-    m.reply(caption)
-    await conn.sendFile(m.chat, url, filename, '', m, null, { mimetype: ext, asDocument: true })
+    await conn.sendFile(m.chat, ss, 'ssweb.png', caption, m)
+    
+    if(!isLimit) await conn.sendFile(m.chat, url, filename, '', m, null, { mimetype: ext, asDocument: true })
     m.react(done)
 }
 handler.help = ['mediafire <url>']
 handler.tags = ['dl', 'prem']
 handler.command = ['mediafire', 'mfire'] 
 handler.diamond = true
-handler.premium = true
+handler.premium = false
 
 export default handler
