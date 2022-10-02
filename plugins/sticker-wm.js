144aa3b88f0ee9a59006471026e772301e2ea2e1
FG98 <81941690+FG98F@users.noreply.github.com>
2022-10-02 18:12:33 -0400

    Update sticker-wm.js

--- plugins/sticker-wm.js
+++ plugins/sticker-wm.js
@@ -1,27 +1,28 @@
 import { addExif } from '../lib/sticker.js'
 
 
-let handler = async (m, { conn, text }) => {
-  if (!m.quoted) throw 'El  sticker!'
+let handler = async (m, { conn, text, args }) => {
+  if (!m.quoted) throw 'Responde a un sticker'
   let stiker = false
+       let stick = args.join(" ").split("|");
+       let f = stick[0] !== "" ? stick[0] : packname;
+       let g = typeof stick[1] !== "undefined" ? stick[1] : author;
   try {
-    let [packname, ...author] = text.split('|')
-    author = (author || []).join('|')
     let mime = m.quoted.mimetype || ''
     if (!/webp/.test(mime)) throw 'Responde a un sticker'
     let img = await m.quoted.download()
     if (!img) throw 'Responde a un sticker!'
-    stiker = await addExif(img, packname || '', author || '')
+    stiker = await addExif(img, f, g)
   } catch (e) {
     console.error(e)
     if (Buffer.isBuffer(e)) stiker = e
   } finally {
-    if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m, false, { asSticker: true })
-    else throw 'La conversión falló'
+    if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m)
+     else throw 'La conversión falló'
   }
 }
 handler.help = ['take <nombre>|<autor>']
 handler.tags = ['sticker']
-handler.command = ['take', 'robar', 'wm'] 
+handler.command = ['take', 'wm'] 
 
 export default handler
