881eece0ff74550826ccd74d05bf88c3a3a9d7b6
FG98 <81941690+FG98F@users.noreply.github.com>
2022-10-02 15:43:49 -0400

    Update nsfw-nsfw.js

--- plugins/nsfw-nsfw.js
+++ plugins/nsfw-nsfw.js
@@ -20,7 +20,7 @@ switch (type) {
         if (!xwai.ok) throw await xwai.text()
         let xwfu = await xwai.json()
         if (!xwfu.url) throw '❎ Error'
-        conn.sendButton(m.chat, `✅ Random *${command}*`, fgyt, xwfu.url, [['▷▷ SIGUIENTE', `${usedPrefix + command}`]],m)
+        conn.sendButton(m.chat, `✅ Random *${command}*`, fgyt, xwfu.url, [['▷▷ SIGUIENTE', `${usedPrefix + command}`]], m)
        m.react(xmoji)    
   break
 
@@ -29,7 +29,7 @@ case 'xneko':
     if (!xnek.ok) throw await xnek.text()
     let xko = await xnek.json()
     if (!xko.url) throw '❎ Error'
-    conn.sendButton(m.chat, `✅ Random *${command}*`, fgyt, xko.url, [['▷▷ SIGUIENTE', `${usedPrefix + command}`]],m)
+    conn.sendButton(m.chat, `✅ Random *${command}*`, fgyt, xko.url, [['▷▷ SIGUIENTE', `${usedPrefix + command}`]], m)
    m.react(xmoji) 
 break
 	
@@ -39,7 +39,7 @@ case 'blowjob':
     if (!res.ok) throw await res.text()
     let json = await res.json()
     if (!json.url) throw '❎ Error'
-    conn.sendButton(m.chat, `✅ Random *${command}*`, fgyt, json.url, [['▷▷ SIGUIENTE', `${usedPrefix + command}`]],m)
+    conn.sendButton(m.chat, `✅ Random *${command}*`, fgyt, json.url, [['▷▷ SIGUIENTE', `${usedPrefix + command}`]], m)
    m.react(xmoji) 
 break
 
@@ -47,41 +47,38 @@ break
 case 'ass':
 case 'culos':
     let xacs = ["https://meme-api.herokuapp.com/gimme/CuteLittleButts", "https://meme-api.herokuapp.com/gimme/ass"]
-    let p = pickRandom(xacs)
-    let as = await axios.get(p)
-    conn.sendButton(m.chat, `✅ Random *${command}*`, fgyt, as.data.url, [['▷▷ SIGUIENTE', `${usedPrefix + command}`]],m)
+    let as = await axios.get(pickRandom(xacs))
+    conn.sendButton(m.chat, `✅ Random *${command}*`, fgyt, as.data.url, [['▷▷ SIGUIENTE', `${usedPrefix + command}`]], m)
    m.react(xmoji) 
 break
 
 case 'boobs':
 case 'boobies':
     let xb = ["https://meme-api.herokuapp.com/gimme/tits", "https://meme-api.herokuapp.com/gimme/BestTits", "https://meme-api.herokuapp.com/gimme/boobs", "https://meme-api.herokuapp.com/gimme/amazingtits", "https://meme-api.herokuapp.com/gimme/TinyTits"]
-    let b = pickRandom(xb)
-    let boo = await axios.get(b)
-    conn.sendButton(m.chat, `✅ Random *${command}*`, fgyt, boo.data.url, [['▷▷ SIGUIENTE', `${usedPrefix + command}`]],m)
+    let boo = await axios.get(pickRandom(xb))
+    conn.sendButton(m.chat, `✅ Random *${command}*`, fgyt, boo.data.url, [['▷▷ SIGUIENTE', `${usedPrefix + command}`]], m)
    m.react(xmoji) 
 break
 
 case 'pussy':
     let xp = ["https://meme-api.herokuapp.com/gimme/pussy", "https://meme-api.herokuapp.com/gimme/LegalTeens"] 
-    let u = pickRandom(xp)
-    let pus = await axios.get(u)
-    conn.sendButton(m.chat, `✅ Random *${command}*`, fgyt, pus.data.url, [['▷▷ SIGUIENTE', `${usedPrefix + command}`]],m)
+    let pus = await axios.get(pickRandom(xp))
+    conn.sendButton(m.chat, `✅ Random *${command}*`, fgyt, pus.data.url, [['▷▷ SIGUIENTE', `${usedPrefix + command}`]], m)
    m.react(xmoji) 
 break
 
 case 'lesbians':
 case 'lesbian':
     let les = await axios.get(`https://meme-api.herokuapp.com/gimme/lesbians`)
-   conn.sendButton(m.chat, `✅ Random *${command}*`, fgyt, les.data.url, [['▷▷ SIGUIENTE', `${usedPrefix + command}`]],m)
+   conn.sendButton(m.chat, `✅ Random *${command}*`, fgyt, les.data.url, [['▷▷ SIGUIENTE', `${usedPrefix + command}`]], m)
    m.react(xmoji) 
 break
 
 case 'pack':
 case 'cosplay':
-	     let img = await axios.get(`https://fg-dylux.herokuapp.com/api/nsfw/pack?apikey=FG98`)
-	     let cosp = img.data.result
-	     conn.sendButton(m.chat, `✅ Resultado 🤭\n Random *${command}*`, 'Vea más fotos aquí \nhttps://t.me/+8SKOTyja8rBlYTlh \n\n', cosp, [['▷▷ SIGUIENTE', `${usedPrefix + command}`]],m)
+	     let img = await conn.getFile(global.API('fgmods', '/api/nsfw/cosplay', { }, 'apikey'))
+	     let cosp = img.data
+	     conn.sendButton(m.chat, `✅ Resultado 🤭\n Random *${command}*`, 'Vea más fotos aquí \nhttps://t.me/+8SKOTyja8rBlYTlh \n\n', cosp, [['▷▷ SIGUIENTE', `${usedPrefix + command}`]], m)
 	     m.react(xmoji) 
 	break
 
