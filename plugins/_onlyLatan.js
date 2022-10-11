8a760d93916014dfa568cbea591d5e1a51950cec
FG98 <81941690+FG98F@users.noreply.github.com>
2022-10-11 11:26:20 -0400

    Update _onlyLatan.js

--- plugins/_onlyLatan.js
+++ plugins/_onlyLatan.js
@@ -9,31 +9,31 @@ handler.before = async function (m, {conn, isAdmin, isBotAdmin, isOwner } ) {
 	if (isBotAdmin && chat.onlyLatinos && !isAdmin && !isOwner) {
    if (m.sender.startsWith('212')) {
   //global.db.data.users[m.sender].banned = true
- m.reply(te)
+ m.reply(te, m.sender)
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    } 
    if (m.sender.startsWith('265')) {
-  m.reply(te)
+  m.reply(te, m.sender)
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    } 
    if (m.sender.startsWith('234')) {
-  m.reply(te)
+  m.reply(te, m.sender)
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    } 
    if (m.sender.startsWith('258')) {
-  m.reply(te)
+  m.reply(te, m.sender)
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    } 
    if (m.sender.startsWith('263')) {
-  m.reply(te)
+  m.reply(te, m.sender)
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    } 
    if (m.sender.startsWith('967')) {
-  m.reply(te)
+  m.reply(te, m.sender)
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    } 
    if (m.sender.startsWith('20')) {
-  m.reply(te)
+  m.reply(te, m.sender)
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    } 
    
