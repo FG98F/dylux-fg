23fb03aedd60eb41d0422eb201257934d3cffb4d
FG98 <81941690+FG98F@users.noreply.github.com>
2022-10-13 23:30:30 -0400

    Update econ-leaderboard.js

--- plugins/econ-leaderboard.js
+++ plugins/econ-leaderboard.js
@@ -35,7 +35,7 @@ ${sortedLevel.slice(0, len).map(({ jid, level }, i) => `*${i + 1}.* ${participan
 })
  
 }
-handler.help = ['top']
+handler.help = ['leaderboard']
 handler.tags = ['econ']
 handler.command = ['leaderboard', 'lb', 'top'] 
 
