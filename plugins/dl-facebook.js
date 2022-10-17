b9b69d70ed57801c4ab35634054709101384c7d1
FG98 <81941690+FG98F@users.noreply.github.com>
2022-10-16 22:52:03 -0400

    Update dl-facebook.js

--- plugins/dl-facebook.js
+++ plugins/dl-facebook.js
@@ -1,17 +1,23 @@
 
-import { facebookdl, facebookdlv2 } from '@bochilteam/scraper'
-
+import fg from 'fg-dylux' 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-	
-    if (!args[0]) throw `✳️ Ingrese un link de un video de Facebook\n\n📌 Ejemplo :\n*${usedPrefix + command}* https://fb.watch/d7nB8-L-gR/`
+ 
+ if (!args[0]) throw `✳️ Ingrese un link de un video de Facebook\n\n📌 Ejemplo :\n*${usedPrefix + command}* https://fb.watch/d7nB8-L-gR/`
     m.react(rwait)
-    try {
-    const { result } = await facebookdl(args[0]).catch(async _ => await facebookdlv2(args[0]))
-    for (const { url, isVideo } of result.reverse()) conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, `✅ Resultado`, m)
-    } catch (e) {
+   try {
+  let res = await fg.fbdl(args[0])
+  let vid = res.data[0].url
+  let cal = res.data[0].quality   
+  let te = `
+┌─⊷ FBDL
+▢ *Calidad:* ${cal}
+└───────────`
+  conn.sendFile(m.chat, vid, 'fb.mp4', te, m)
+  m.react(done)
+  } catch (e) {
 	m.reply('❎ Ocurrió un error Revisa que link sea correcto')
 	} 
-    
+	
 }
 handler.help = ['facebook'].map(v => v + ' <url>')
 handler.tags = ['dl']
