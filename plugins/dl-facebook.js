cdf5199969d392fdda0efb9e7008b819b3cf5db3
FG98 <81941690+FG98F@users.noreply.github.com>
2022-10-28 13:15:06 -0400

    Update dl-facebook.js

--- plugins/dl-facebook.js
+++ plugins/dl-facebook.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
  
  if (!args[0]) throw `✳️ ${msg.noLinkvid('Facebook')}\n\n📌 ${msg.example()} :\n*${usedPrefix + command}* https://fb.watch/d7nB8-L-gR/`
     m.react(rwait)
-   
+   try {
    let res = await fg.fbdl(args[0])
     for (let result of res.download) {
     	  let tex = `
@@ -12,8 +12,11 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 ▢ *Calidad:* ${result.quality}
 └───────────`
     conn.sendFile(m.chat, result.url, 'fb.mp4', tex, m)
-} 
-	m.react(done) 
+     } 
+     m.react(done)
+ } catch {
+ 	m.reply('Error: Intente de nuevo con otro link')
+ 	} 
 }
 handler.help = ['facebook'].map(v => v + ' <url>')
 handler.tags = ['dl']
