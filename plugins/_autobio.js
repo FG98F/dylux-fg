4d439faac3051dba69cc9a95a899910658eedbd4
FG98 <81941690+FG98F@users.noreply.github.com>
2022-10-30 01:26:24 -0400

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -12,7 +12,7 @@ let _muptime
       }) * 1000
     }
     let muptime = clockString(_muptime)
-		let bio = `\n🟢 Tiempo Activo ${muptime}\n\n┃ 💎  By FG98`
+		let bio = `\n🟢 Tiempo Activo ${muptime}\n\n ┃ 💎  By FG98`
 		await this.updateProfileStatus(bio).catch(_ => _)
 		setting.status = new Date() * 1
 
