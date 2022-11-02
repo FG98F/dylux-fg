aaebc484423a7a574315c8f4247c71798f6d14ac
FG98 <81941690+FG98F@users.noreply.github.com>
2022-11-02 13:41:02 -0400

    Update dl-twitter.js

--- plugins/dl-twitter.js
+++ plugins/dl-twitter.js
@@ -1,22 +1,24 @@
-import twitterDl from 'fg-twitter'
 
+import fg from 'api-dylux'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `✳️ Ejemplo : \n${usedPrefix + command} https://twitter.com/fernandavasro/status/1569741835555291139?t=ADxk8P3Z3prq8USIZUqXCg&s=19`
-    let res = await twitterDl(args[0])
-    try {
-    for (let result of res.download) {
-    	let res = result.url.replace('https://ssstwitter.com', '')
-       conn.sendFile(m.chat, res, 'twitter.mp4', '✅ Resultado' , m)
-  }
-  } catch (e) {
-  	m.reply('✳️ Revise el link sea de un video de Twitter')
+if (!args[0]) throw `📌 Ejemplo : \n*${usedPrefix + command}* https://twitter.com/fernandavasro/status/1569741835555291139?t=ADxk8P3Z3prq8USIZUqXCg&s=19`
+          m.react(rwait)    
+          try {
+          let { SD, HD, desc, thumb, audio } = await fg.twitter(args[0])
+          let te = ` 
+┌─⊷ *TWITTER DL*
+▢ Descripción: ${desc}
+└───────────`
+conn.sendFile(m.chat, HD, 'twitter.mp4', te, m)
+m.react(done)
+} catch (e) {
+  	m.reply(`✳️ verifica que el link sea de Twitter`)
 	} 
-  
+	
 }
 handler.help = ['twitter'].map(v => v + ' <url>')
 handler.tags = ['dl']
 handler.command = /^(twitter|tw)$/i
-
 handler.diamond = true
 
 export default handler
