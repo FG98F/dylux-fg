c00a09e6e5f0fa0941b2259b7d90a1ed4d1a4034
FG98 <81941690+FG98F@users.noreply.github.com>
2022-11-19 09:31:55 -0400

    Update dl-play2.js

--- plugins/dl-play2.js
+++ plugins/dl-play2.js
@@ -1,17 +1,17 @@
 
 import { youtubeSearch } from '@bochilteam/scraper'
-
+import yts from 'yt-search'
 let handler = async(m, { conn, usedPrefix, text, args, command }) => {
 
     if (!text) throw `✳️ Ingresa el título de una canción\n\n*📌 Ejemplo*\n*${usedPrefix + command}* Lil Peep hate my fuccn life `
     m.react('📀')
-    let result = await youtubeSearch(text)
-    let ytres = result.video
+    let result = await yts(text)
+    let ytres = result.all
     let listSections = []
 	Object.values(ytres).map((v, index) => {
 	listSections.push([`${index}┃ ${v.title}`, [
-          ['🎶 MP3', `${usedPrefix}fgmp3 ${v.url} yes`, `▢ ⌚ *Duración:* ${v.durationH}\n▢ 👀 *Vistas:* ${v.view}\n▢ 📌 *Título* : ${v.title}\n▢ 📆 *Publicado:* ${v.publishedTime}\n`],
-          ['🎥 MP4', `${usedPrefix}fgmp4 ${v.url} yes`, `▢ ⌚ *Duración:* ${v.durationH}\n▢ 👀 *Vistas:* ${v.view}\n▢ 📌 *Título* : ${v.title}\n▢ 📆 *Publicado:* ${v.publishedTime}\n`]
+          ['🎶 MP3', `${usedPrefix}fgmp3 ${v.url} yes`, `▢ ⌚ *Duración:* ${v.timestamp}\n▢ 👀 *Vistas:* ${v.views}\n▢ 📌 *Título* : ${v.title}\n▢ 📆 *Publicado:* ${v.ago}\n`],
+          ['🎥 MP4', `${usedPrefix}fgmp4 ${v.url} yes`, `▢ ⌚ *Duración:* ${v.timestamp}\n▢ 👀 *Vistas:* ${v.views}\n▢ 📌 *Título* : ${v.title}\n▢ 📆 *Publicado:* ${v.ago}\n`]
         ]])
 	})
 	return conn.sendList(m.chat, '  ≡ *FG MUSIC*🔎', `\n 📀 Aqui una lista de resultados de :\n *${text}*`, igfg, `Click Aquí `, listSections, m)
