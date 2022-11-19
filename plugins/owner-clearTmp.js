87d0dd48b49a70d46aed815bb4fae41b09d64a88
FG98 <81941690+FG98F@users.noreply.github.com>
2022-11-19 09:50:43 -0400

    Update owner-clearTmp.js

--- plugins/owner-clearTmp.js
+++ plugins/owner-clearTmp.js
@@ -3,29 +3,31 @@ import { tmpdir } from 'os'
 import path, { join } from 'path'
 import {
   readdirSync,
-  statSync,
   unlinkSync,
-  existsSync,
-  readFileSync,
-  watch
+  rmSync
 } from 'fs'
 
 let handler = async (m, { conn, __dirname, args }) => {
 
-conn.reply(m.chat, '✅ Se limpió la carpeta *tmp*', m)
+m.reply(`✅ Se limpió la carpeta *tmp + sessions*`)
 m.react(done)
-
 const tmp = [tmpdir(), join(__dirname, '../tmp')]
   const filename = []
   tmp.forEach(dirname => readdirSync(dirname).forEach(file => filename.push(join(dirname, file))))
+ 
+  //session bot
+  readdirSync("./sessions").forEach(file => {
+    if (file !== 'creds.json') {
+        unlinkSync("./sessions/" + file, { recursive: true, force: true })}}) 
+      
   return filename.map(file => {
-    const stats = statSync(file)
     unlinkSync(file)
 })
+
 }
 handler.help = ['cleartmp']
 handler.tags = ['owner']
 handler.command = /^(cleartmp)$/i
-//handler.rowner = true
+handler.rowner = true
 
 export default handler
