e0b5fcd913903bc7ff2f4ae85e83e69978ccb936
FG98 <81941690+FG98F@users.noreply.github.com>
2022-11-19 09:18:12 -0400

    Update dl-play.js

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -1,31 +1,28 @@
 
-import fetch from 'node-fetch'
-import { youtubeSearch } from '@bochilteam/scraper'
-
+import yts from 'yt-search'
 let handler = async (m, { conn, command, text, usedPrefix }) => {
-  if (!text) throw `✳️ *Ingresa el título de una canción*\n\n📌Ejemplo *${usedPrefix + command}* Lil Peep hate my life `
-  let vid = (await youtubeSearch(text)).video[0]
-  if (!vid) throw '✳️ Vídeo/Audio no encontrado'
-  let { title, description, thumbnail, videoId, durationH, viewH, publishedTime } = vid
-  const url = 'https://www.youtube.com/watch?v=' + videoId
-  
-m.react('🎧')
- await conn.sendButton(m.chat, `
-  ≡ *FG MUSIC*
-┌──────────────
-▢ 📌 *Título* : ${title}
-▢ 📆 *Publicado:* ${publishedTime}
-▢ ⌚ *Duración:* ${durationH}
-▢ 👀 *Vistas:* ${viewH}
-└──────────────
-  `.trim(), igfg, thumbnail, [
-    ['🎶 MP3', `${usedPrefix}fgmp3 ${url} yes`],
+	
+	if (!text) throw `✳️ *Ingresa el título de una canción*\n\n📌Ejemplo *${usedPrefix + command}* Lil Peep hate my life`
+	let vid = (await yts(text)).all[0]
+	if (!vid) throw `✳️ Vídeo/Audio no encontrado`
+	let { title, description, thumbnail, videoId, timestamp, views, ago, url } = vid
+	//const url = 'https://www.youtube.com/watch?v=' + videoId
+	m.react('🎧')
+	let play = `
+	≡ *FG MUSIC*
+  ┌──────────────
+  ▢ 📌 *Título* : ${title}
+  ▢ 📆 *Publicado:* ${ago}
+  ▢ ⌚ *Duración:* ${timestamp}
+  ▢ 👀 *Vistas:* ${views}
+  └──────────────`
+ await conn.sendButton(m.chat, play, igfg, thumbnail, [
+    ['🎶 MP3', `${usedPrefix}fgmp3 ${url}`],
     ['🎥 MP4', `${usedPrefix}fgmp4 ${url}`]
   ], m, rpl)
 }
 handler.help = ['play']
 handler.tags = ['dl']
-handler.command = ['play', 'playvid'] 
+handler.command = ['play', 'playvid']
 
 export default handler
-
