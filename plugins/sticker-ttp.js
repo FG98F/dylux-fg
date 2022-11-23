301355e76cc70e31c9197d890f1af905921f72ee
FG98 <81941690+FG98F@users.noreply.github.com>
2022-11-23 09:25:04 -0400

    Update sticker-ttp.js

--- plugins/sticker-ttp.js
+++ plugins/sticker-ttp.js
@@ -1,17 +1,11 @@
+
 import { sticker } from '../lib/sticker.js'
-import fetch from 'node-fetch'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw `✳️ _Envie el texto_\n\n📌Ejemplo *${usedPrefix + command}* dylux-fg`  
-  try {
-    let url = await fetch(global.API('https://salism3api.pythonanywhere.com', '/text2img/', { text } ))
-    let res = await url.json()
-    let stick = res.image
-    let stiker = await sticker(null, stick, global.packname, global.author)
+    
+    if (!text) throw `✳️ Envie el texto\n\n📌Ejemplo *${usedPrefix + command}* dylux-fg`  
+    let stiker = await sticker(null, global.API('xteam', '/ttp', { file: '', text: text }), global.packname, global.author)
     if (stiker) return await conn.sendFile(m.chat, stiker, '', '', m, null, rpl)
-  } catch (e) {
-    m.reply('Conversión archivada')
-    throw false
-  }
+    throw stiker.toString()
 }
 handler.help = ['ttp <text>']
 handler.tags = ['sticker']
