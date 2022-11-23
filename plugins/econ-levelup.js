142acd2b7565652e79cb4eaa354afe9a104c162a
FG98 <81941690+FG98F@users.noreply.github.com>
2022-11-23 12:24:12 -0400

    Update econ-levelup.js

--- plugins/econ-levelup.js
+++ plugins/econ-levelup.js
@@ -1,13 +1,12 @@
-import { canLevelUp, xpRange } from '../lib/levelling.js'
-import { levelup } from '../lib/canvas.js'
-//import db from '../lib/database.js'
 
+import { canLevelUp, xpRange } from '../lib/levelling.js'
 let handler = async (m, { conn }) => {
-	let name = conn.getName(m.sender)
+	  let name = conn.getName(m.sender)
+    let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://i.imgur.com/whjlJSf.jpg')
     let user = global.db.data.users[m.sender]
     if (!canLevelUp(user.level, user.exp, global.multiplier)) {
         let { min, xp, max } = xpRange(user.level, global.multiplier)
-        throw `
+        let txt = `
 ┌───⊷ *NIVEL*
 ▢ Nombre : *${name}*
 ▢ Nivel : *${user.level}*
@@ -17,12 +16,26 @@ let handler = async (m, { conn }) => {
 
 Te falta *${max - user.exp}* de *XP* para subir de nivel
 `.trim()
+try {
+  let imgg = API('fgmods', '/api/rank', {
+    username: name,
+    xp: user.exp - min,
+    exp: xp,
+    avatar: pp,
+    level: user.level,
+    background: 'https://i.ibb.co/CsNgBYw/qiyana.jpg'
+}, 'apikey')
+
+    conn.sendFile(m.chat, imgg, 'level.jpg', txt, m)
+} catch (e) {
+    m.reply(txt)
+}
     }
     let before = user.level * 1
     while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
     if (before !== user.level) {
     	user.role = global.rpg.role(user.level).name
-        let teks = `Bien hecho ${conn.getName(m.sender)} Nivel:`
+
         let str = `
 ┌─⊷ *LEVEL UP*
 ▢ Nivel anterior : *${before}*
@@ -33,8 +46,10 @@ Te falta *${max - user.exp}* de *XP* para subir de nivel
 *_Cuanto más interactúes con los bots, mayor será tu nivel_*
 `.trim()
         try {
-            const img = await levelup(teks, user.level)
-            conn.sendFile(m.chat, img, 'levelup.jpg', str, m)
+            let img = API('fgmods', '/api/levelup', { 
+                avatar: pp 
+             }, 'apikey')
+      conn.sendFile(m.chat, img, 'levelup.jpg', str, m)
         } catch (e) {
             m.reply(str)
         }
