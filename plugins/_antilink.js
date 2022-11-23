a76392816e8679351c6ff64029e7eb9b19200bd4
FG98 <81941690+FG98F@users.noreply.github.com>
2022-11-23 09:29:04 -0400

    Add files via upload

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,4 +1,3 @@
-//import db from '../lib/database.js'
 
 const linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
 
@@ -8,8 +7,6 @@ export async function before(m, {conn, isAdmin, isBotAdmin }) {
     if (!m.isGroup) return !1
     let chat = global.db.data.chats[m.chat]
     let bot = global.db.data.settings[this.user.jid] || {}
-    let delet = m.key.participant
-    let bang = m.key.id
     const isGroupLink = linkRegex.exec(m.text)
 
     if (chat.antiLink && isGroupLink && !isAdmin) {
@@ -22,7 +19,7 @@ export async function before(m, {conn, isAdmin, isBotAdmin }) {
 No permitimos enlaces de otros grupos 
 lo siento *@${m.sender.split('@')[0]}*  serás expulsado del grupo ${isBotAdmin ? '' : '\n\nNo soy admin así que no te puedo expulsar :"v'}`, null, { mentions: [m.sender] } )
         if (isBotAdmin && chat.antiLink) {
-        	await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
+        	await conn.sendMessage(m.chat, { delete: m.key })
             await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
         } else if (!chat.antiLink) return //m.reply('')
     }
