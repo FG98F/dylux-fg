659d56599aa6868507acb432959b071279a3d6e8
FG98 <81941690+FG98F@users.noreply.github.com>
2022-11-28 18:45:09 -0400

    Add files via upload

--- plugins/gp-link.js
+++ plugins/gp-link.js
@@ -1,18 +1,15 @@
-import { areJidsSameUser } from '@adiwajshing/baileys'
-let handler = async (m, { conn, args }) => {
-    let group = m.chat
-    if (/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(args[0])) group = args[0]
-    if (!/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(group)) throw '⚠️ Sólo se puede usar en grupos'
-    let groupMetadata = await conn.groupMetadata(group)
-    if (!groupMetadata) throw 'groupMetadata is undefined :\\'
-    if (!('participants' in groupMetadata)) throw 'participants no está definido :('
-    let me = groupMetadata.participants.find(user => areJidsSameUser(user.id, conn.user.id))
-    if (!me) throw '✳️ No estoy en ese grupo :('
-    if (!me.admin) throw '✳️ No soy administrador'
-    m.reply('https://chat.whatsapp.com/' + await conn.groupInviteCode(group))
+
+let handler = async (m, { conn, groupMetadata }) => { 
+
+ let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(m.chat)
+ //conn.reply(m.chat, `\n${msg.gpLink()} *${groupMetadata.subject}*\n\n${link}`, m, {detectLink: true})
+conn.sendHydrated(m.chat, `\n${msg.gpLink()} *${groupMetadata.subject}*\n\n${link}`, packname, null, 'https://www.whatsapp.com/otp/copy/https://chat.whatsapp.com/' + await conn.groupInviteCode(m.chat), 'Copiar', null, null, [ null ], m)
+
 }
 handler.help = ['link']
 handler.tags = ['group']
-handler.command = ['link', 'linkgroup'] 
+handler.command = ['linkgroup', 'link'] 
+handler.group = true
+handler.botAdmin = true
 
 export default handler
