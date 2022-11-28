e180ccebb29a46c997d65efa95d79669c045c37a
FG98 <81941690+FG98F@users.noreply.github.com>
2022-11-28 18:59:06 -0400

    Update dl-gdrive.js

--- plugins/dl-gdrive.js
+++ plugins/dl-gdrive.js
@@ -1,35 +1,27 @@
 
 import fg from 'api-dylux' 
-let free = 150 // limite de descarga
-let prem = 500
-let handler = async (m, { conn, args, usedPrefix, command, isOwner, isPrems }) => {
+let handler = async (m, { conn, args, usedPrefix, command }) => {
 
-	if (!args[0]) throw `✳️ ${msg.noLink('Google Drive')}`
+	if (!args[0]) throw `✳️ Ingrese un link de Google Drive`
 	m.react(rwait) 
 	try {
 	let res = await fg.GDriveDl(args[0])
-
-	let limit = isPrems || isOwner ? prem : free
-	if (res.fileSize.split('MB')[0] >= limit) return m.reply(`▢ ${msg.limitdl()} *+${free} MB* ${msg.limitdlTe()} *${prem} MB*`) 
-	if (res.fileSize.includes('GB')) return m.reply(`▢ ${msg.limitdl()} *+${free} MB* ${msg.limitdlTe()} *${prem} MB*`)
-
 	 await m.reply(`
 ≡ *Google Drive DL*
-
-▢ *${msg.lname()}:* ${res.fileName}
-▢ *${msg.lsize()}:* ${res.fileSize}
-▢ *${msg.type()}:* ${res.mimetype}`)
+▢ *Nombre:* ${res.fileName}
+▢ *Tamaño:* ${res.fileSize}
+▢ *tipo:* ${res.mimetype}`)
 		
 	conn.sendMessage(m.chat, { document: { url: res.downloadUrl }, fileName: res.fileName, mimetype: res.mimetype }, { quoted: m })
 	m.react(done)
    } catch {
-	m.reply(msg.errorDl()) 
+	m.reply('Error: Revisa el link o intenta con otro link') 
   }
 }
 handler.help = ['gdrive']
 handler.tags = ['dl', 'prem']
 handler.command = ['gdrive']
 handler.diamond = true
-//handler.premium = true
+handler.premium = true
 
 export default handler
