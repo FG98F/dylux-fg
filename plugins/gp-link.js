1c0e47efe634120af4d2eb39fe5def438dcd0740
FG98 <81941690+FG98F@users.noreply.github.com>
2022-11-28 18:51:27 -0400

    Update gp-link.js

--- plugins/gp-link.js
+++ plugins/gp-link.js
@@ -1,6 +1,6 @@
 import { areJidsSameUser } from '@adiwajshing/baileys'
 let handler = async (m, { conn, args }) => {
-    let group = m.chat
+    let group = m.chat 
     if (/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(args[0])) group = args[0]
     if (!/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(group)) throw '⚠️ Sólo se puede usar en grupos'
     let groupMetadata = await conn.groupMetadata(group)
