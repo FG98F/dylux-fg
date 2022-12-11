01ac12fea3c8f6f269d95020b4ce13c8a3395563
FG98 <81941690+FG98F@users.noreply.github.com>
2022-12-10 23:05:52 -0400

    Update main-dylux.js

--- plugins/main-dylux.js
+++ plugins/main-dylux.js
@@ -1,36 +1,35 @@
 
 let handler = async (m, { conn }) => {
 
-    m.reply(`
-    ≡  *DyLuxᴮᴼᵀ ┃ SUPPORT*
-    
-    ◈ ━━━━━━━━━━━━━━━━━━━━ ◈
-    ▢ Grupo *1*
-    https://chat.whatsapp.com/IO9jmpI72ejHiN4unRZleU
-    
-    ▢ Grupo *2*
-    https://chat.whatsapp.com/CDUqNRu5Kh5KY5uqQI0BKE
-    
-    ▢ Grupo *NSFW* 🔞
-    https://chat.whatsapp.com/F0JTTyZ3hsoL7OlU8TEpuH
-    
-    ◈ ━━━━━━━━━━━━━━━━━━━━ ◈
-    ▢ Todos los Grupos
-     https://instabio.cc/fg98ff
-    
-    ▢ *Telegram*
-    • https://t.me/fgawgp
-    
-     ▢ *PayPal*
-    • https://paypal.me/fg98f
-    
-    ▢ *YouTube*
-    • https://www.youtube.com/fg98f`)
-    
-    }
-    handler.help = ['support']
-    handler.tags = ['main']
-    handler.command = ['grupos', 'groupdylux', 'dxgp', 'dygp', 'gpdylux', 'support'] 
-    
-    export default handler
-    
+m.reply(`
+≡  *DyLuxᴮᴼᵀ ┃ SUPPORT*
+
+◈ ━━━━━━━━━━━━━━━━━━━━ ◈
+▢ Grupo *1*
+https://chat.whatsapp.com/IO9jmpI72ejHiN4unRZleU
+
+▢ Grupo *2*
+https://chat.whatsapp.com/CDUqNRu5Kh5KY5uqQI0BKE
+
+▢ Grupo *NSFW* 🔞
+https://chat.whatsapp.com/F0JTTyZ3hsoL7OlU8TEpuH
+
+◈ ━━━━━━━━━━━━━━━━━━━━ ◈
+▢ Todos los Grupos
+ https://instabio.cc/fg98ff
+
+▢ *Telegram*
+• https://t.me/fgawgp
+
+ ▢ *PayPal*
+• https://paypal.me/fg98f
+
+▢ *YouTube*
+• https://www.youtube.com/fg98f`)
+
+}
+handler.help = ['support']
+handler.tags = ['main']
+handler.command = ['grupos', 'groupdylux', 'dxgp', 'dygp', 'gpdylux', 'support'] 
+
+export default handler
