dcce9e998a424b9e034067be3fc22809415257f0
FG98 <81941690+FG98F@users.noreply.github.com>
2022-12-26 06:42:21 -0400

    Update main-donate.js

--- plugins/main-donate.js
+++ plugins/main-donate.js
@@ -9,8 +9,6 @@ puedes donar si quieres ayudar a mantener el bot activo
 • *Link :* https://paypal.me/fg98f
 ▢ *Mercado Pago Arg*
 • *Link :* https://mpago.la/1F3r6JH
-▢ *Tigo Money*
-• *N° :*  75140648 
 `
 let img = 'https://i.ibb.co/37FP2bk/donate.jpg'
 conn.sendButton(m.chat, don, 'Haga click en boton si quiere comprar o alquilar al bot', img, [['Buy Premium', `${usedPrefix}buyprem`]],m, rpyp)
