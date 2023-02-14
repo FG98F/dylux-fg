940e352121e1317974952bba3cdce3b33e06b537
botzeroe <124211008+botzeroe@users.noreply.github.com>
2023-02-14 05:52:46 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['59172945992', 'FG98', true],
-  ['5492622271736'], 
-  ['5492622271736'] 
+  ['967776008370', 'Ze0rO0sama', true],
+  ['967776008370'], 
+  ['967776008370'] 
 ] //Numeros de owner 
 
-global.mods = ['573125484672'] 
-global.prems = ['50489079501', '5219631769130', '573125484672']
+global.mods = ['967776008370'] 
+global.prems = ['967776008370', '967776008370', '967776008370']
 global.APIs = { // API Prefix
   // name: 'https://website'
   xteam: 'https://api.xteam.xyz', 
@@ -25,13 +25,13 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'DyLux┃ᴮᴼᵀ' 
-global.author = '@fg98' 
-global.fgig = '▢ Sígueme en Instagram\nhttps://www.instagram.com/fg98_ff\n' 
-global.dygp = 'https://chat.whatsapp.com/IO9jmpI72ejHiN4unRZleU'
+global.packname = '𖤍𓆩ɀꫀ𝘳ꪮ | ࢪ࣪يࢪو𓆪𖤍' 
+global.author = '@Ze0rO0sama' 
+global.fgig = '▢ Sígueme en Instagram\nhttps://instagram.com/ze0ro0sama\n' 
+global.dygp = 'https://chat.whatsapp.com/JYEBaqSmFKCC5sbvSoFZQd'
 global.fgsc = 'https://github.com/FG98F/dylux-fg' 
-global.fgyt = 'https://youtube.com/fg98f'
-global.fgpyp = 'https://paypal.me/fg98f'
+global.fgyt = 'https://www.youtube.com/@Ze0rO0sama'
+global.fgpyp = 'https://paypal.me/Ze0rO0sama'
 global.fglog = 'https://i.imgur.com/Owmb93c.png' 
 
 global.wait = '*⌛ _Cargando..._*\n*▰▰▰▱▱▱▱▱*'
