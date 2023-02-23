eaa9fc7390450b54fcb3df676d40ea3c487647b6
FG98 <81941690+FG98F@users.noreply.github.com>
2023-02-22 21:05:34 -0300

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ global.prems = ['50489079501', '5219631769130', '573125484672']
 global.APIs = { // API Prefix
   // name: 'https://website'
   xteam: 'https://api.xteam.xyz', 
-  nrtm: 'https://fg-nrtm-nhie.onrender.com',
+  nrtm: 'https://fg-nrtm.ddns.net',
   bg: 'http://bochil.ddns.net',
   fgmods: 'https://api-fgmods.ddns.net'
 }
