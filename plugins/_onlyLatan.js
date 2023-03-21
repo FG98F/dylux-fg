f88232b6f717abbc23bde3cc91d91035d75e9e49
FG98 <81941690+FG98F@users.noreply.github.com>
2023-03-21 15:51:12 -0300

    Update _onlyLatan.js

--- plugins/_onlyLatan.js
+++ plugins/_onlyLatan.js
@@ -1,43 +1,24 @@
 
 let handler = m => m
 
-handler.before = async function (m, {conn, isAdmin, isBotAdmin, isOwner } ) {
-	
-	if (!m.isGroup) return !1
-	let chat = global.db.data.chats[m.chat]
-	let te = `✳️ En este grupo solo se permite personas de habla hispana`
-	if (isBotAdmin && chat.onlyLatinos && !isAdmin && !isOwner) {
-   if (m.sender.startsWith('212')) {
-  //global.db.data.users[m.sender].banned = true
- m.reply(te, m.sender)
-conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-   } 
-   if (m.sender.startsWith('265')) {
-  m.reply(te, m.sender)
-conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-   } 
-   if (m.sender.startsWith('234')) {
-  m.reply(te, m.sender)
-conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-   } 
-   if (m.sender.startsWith('258')) {
-  m.reply(te, m.sender)
-conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-   } 
-   if (m.sender.startsWith('263')) {
-  m.reply(te, m.sender)
-conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-   } 
-   if (m.sender.startsWith('967')) {
-  m.reply(te, m.sender)
-conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-   } 
-   if (m.sender.startsWith('20')) {
-  m.reply(te, m.sender)
-conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-   } 
-   
-   //---
-}  
+handler.before = async function (m, {conn, isAdmin, isBotAdmin, isOwner} ) {
+  if (!m.isGroup) return !1
+  let chat = global.db.data.chats[m.chat];
+  
+  if (isBotAdmin && chat.onlyLatinos && !isAdmin && !isOwner) {
+    let forbidPrefixes = ["212", "265", "234", "258", "263", "967", "20", "92", "91"];
+
+    for (let prefix of forbidPrefixes) {
+      if (m.sender.startsWith(prefix)) {
+        m.reply('✳️ En este grupo solo se permite personas de habla hispana', m.sender)
+        await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
+        return false;
+      }
+    }
+  }
+  
+  return true;
 }
-export default handler
+
+
+export default handler;
