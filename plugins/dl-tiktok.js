584aa30b2a28018753abc73ce98c1169516c6196
FG98 <81941690+FG98F@users.noreply.github.com>
2023-03-31 16:48:03 -0300

    Update dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -14,7 +14,7 @@ try {
 ▢ *Username:* ${p.author}
 ▢ *Descripción:* ${p.title}
 └───────────`
-    conn.sendButton(m.chat, te, fgig, p.nowm, [['⎘ Stalkig', `${usedPrefix}ttstalk ${p.author.replace(/^@/, '')}`], ['♫ Audio', `${usedPrefix}tomp3`]], m)
+    conn.sendFile(m.chat, p.nowm, 'tt.mp4', te, m)
     m.react(done)
     } catch {  	
     try { 
@@ -23,10 +23,10 @@ try {
          .catch(async _ => await tiktokdlv3(args[0]))
     const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd
     if (!url) throw '❎ Error al descargar el video'
-    conn.sendButton(m.chat, `
-┌─⊷ TIKTOK
+    conn.sendFile(m.chat, url, 'fb.mp4', `
+┌─⊷ *TIKTOK DL-2*
 ▢ *Nickname:* ${nickname} ${description ? `\n▢ *Descripción:* ${description}` : ''}
-└───────────`, fgig, url, [['♫ Audio', `${usedPrefix}tomp3`]], m)
+└───────────`, m)
 m.react(done)
 } catch {
     m.reply(`❎ Error al descargar el video`)
