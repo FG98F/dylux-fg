a47b69f3c18a799c09c95f5cb2cd3d439ab72ff4
FG98 <81941690+FG98F@users.noreply.github.com>
2023-04-05 10:57:23 -0300

    Update sticker-emojimix.js

--- plugins/sticker-emojimix.js
+++ plugins/sticker-emojimix.js
@@ -1,23 +1,12 @@
 
 import fetch from 'node-fetch'
 import { sticker } from '../lib/sticker.js'
-
-const fetchJson = (url, options) => new Promise(async (resolve, reject) => {
-fetch(url, options)
-.then(response => response.json())
-.then(json => {
-resolve(json)
-})
-.catch((err) => {
-reject(err)
-})})
-
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 	
 if (!args[0]) throw `📌 Ejemplo : ${usedPrefix + command} 😎+🤑`
 if (!text.includes('+')) throw  `✳️ Separe el emoji con un *+* \n\n📌 Ejemplo : \n*${usedPrefix + command}* 😎+🤑`
 let [emoji, emoji2] = text.split`+`
-let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji)}_${encodeURIComponent(emoji2)}`)
+let anu = await (await fetch(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji)}_${encodeURIComponent(emoji2)}`) ).json()  
 for (let res of anu.results) {
 let stiker = await sticker(false, res.url, global.packname, global.author)
 conn.sendFile(m.chat, stiker, null, { asSticker: true }, m)
