44919ebc33f2e66bbfc2ae07377751206a15e4e2
FG98 <81941690+FG98F@users.noreply.github.com>
2023-04-25 18:23:46 -0300

    Update nsfw-xnxxdl.js

--- plugins/nsfw-xnxxdl.js
+++ plugins/nsfw-xnxxdl.js
@@ -1,54 +1,49 @@
 
 import fetch from 'node-fetch'
-import axios from 'axios'
-let handler = async (m, {conn, text, usedPrefix, command }) => {
-  let chat = global.db.data.chats[m.chat]
-  if (!chat.nsfw) throw `🚫 El grupo no admite contenido nsfw \n\n Para habilitar escriba \n*${usedPrefix}enable* nsfw`
+import fg from 'api-dylux'
+let handler = async (m, { conn, args, text, usedPrefix, command }) => {
+
+ let chat = global.db.data.chats[m.chat]
+  if (!chat.nsfw) throw `🚫 El grupo no admite contenido nsfw \n\nPara habilitar escriba \n*${usedPrefix}enable* nsfw`
   let user = global.db.data.users[m.sender].age
   if (user < 17) throw `❎ Eres menor de edad! vuelve cuando tengas más de 18 años`
-  if (!text) throw `✳️ Para buscar\n📌 Use : *${usedPrefix}xnxx <search>*\n\nPara descargar desde URL:\n📌Use : *${usedPrefix}xnxxdl <url>*`
- m.react(rwait)
-
-let type = (command).toLowerCase()
-switch (type) {
-	case 'xnxxsearch':
-  case 'xnxx':
-  //if (!text) return m.reply(`✳️ ${msg.search()}`)
-  try {
-  let res = await fetch(global.API('fgmods', '/api/xnxxsearch', { q: text }, 'apikey'))
-  let json = await res.json()
-   let listSections = []
-	Object.values(json.result).map((v, index) => {
-	listSections.push([`${index}┃ ${v.title}`, [
-          ['🎥 MP4', `${usedPrefix}xnxxdl ${v.link}`, `▢ 📌 *Título* : ${v.title}`]
-        ]])
-	})
-	return conn.sendList(m.chat, '  ≡ *XNXX DL*🔎', `\n 🔞 Resultados de:\n *${text}*`, fgig, `Click Aquí`, listSections, m)
-	} catch (e) {
-    m.reply(`🔴 Error: intenta con otro`)
-     }
-  break
-  case 'xnxxdl':
-  // if (!text) return m.reply(`✳️ ${msg.example()} :\n${usedPrefix + command} https://www.xnxx.com/video-9q1wbf7/full_version_https_is.gd_utbgur_cute_sexy_japanese_amature_girl_sex_adult_douga`)
-   try {
-  let xn = await (await fetch(global.API('fgmods', '/api/xnxxdl', { url: text }, 'apikey'))).json()
-  conn.sendFile(m.chat, xn.result.files.high, xn.result.title + '.mp4', `
- ≡  *XNXX DL*
-  
+  if (!text) throw `✳️ Para buscar\n📌 Use : *${usedPrefix + command} <search>*\n\nPara descargar desde URL:\n📌Use : *${usedPrefix + command} <url>*`
+    
+    m.react(rwait)
+    if (text.includes('http://') || text.includes('https://')) {
+        if (!text.includes('xnxx.com')) return m.reply(`❎ Ingrese un link de *xnxx.com*`)
+        try {
+            let xn = await (await fetch(global.API('fgmods', '/api/xnxxdl', { url: text }, 'apikey'))).json()
+            conn.sendFile(m.chat, xn.result.files.high, xn.result.title + '.mp4', `
+≡  *XNXX DL*
+            
 ▢ *📌Título*: ${xn.result.title}
 ▢ *⌚Duración:* ${xn.result.duration}
 ▢ *🎞️Calidad:* ${xn.result.quality}
 `.trim(), m, false, { asDocument: chat.useDocument })
  m.react(done)
  } catch (e) {
-m.reply(`🔴 Error : intenta con otro link`)
+    m.reply(`🔴 Error : intenta mas tarde`)
+ }
+    } else {
+        try {
+            let res = await fetch(global.API('fgmods', '/api/xnxxsearch', { q: text }, 'apikey'))
+            let json = await res.json()
+             let listSections = []
+              Object.values(json.result).map((v, index) => {
+              listSections.push([`${index}┃ ${v.title}`, [
+                    ['🎥 MP4', `${usedPrefix}xnxxdl ${v.link}`, `▢ 📌 *Título* : ${v.title}`]
+                  ]])
+              })
+              //return conn.sendList(m.chat, '  ≡ *XNXX DL*🔎', `\n 🔞 Resultados de:\n *${text}*`, fgig, `Click Aquí`, listSections, m)
+              let ff = json.result.map((v, i) => `${i + 1}┃ *Titulo* : ${v.title}\n*Link:* ${v.link}\n`).join('\n') 
+              if (json.status) m.reply(ff)
+            } catch (e) {
+              m.reply(`🔴 Error: intenta mas tarde`)
+               }
+    }
 }
-  break
-
-default:
-} 
-}
-handler.help = ['xnxx 🔎', 'xnxxdl <link>'] 
+handler.help = ['xnxx'] 
 handler.tags = ['nsfw', 'prem']
 handler.command = ['xnxxsearch', 'xnxxdl', 'xnxx'] 
 handler.diamond = 2
