57d3915ae3501f79868088a29d794bb1fd6524e9
kanixbot <122229140+kanixbot@users.noreply.github.com>
2023-06-09 23:18:49 -0500

    Add files via upload

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -12,7 +12,7 @@ let _muptime
       }) * 1000
     }
     let muptime = clockString(_muptime)
-		let bio = `\n🟢 Tiempo Activo ${muptime}\n\n ┃ 💎  By FG98`
+		let bio = `\n🍁El Bot Más Piola De Whatsapp Activó En ${muptime}\n\n ┃ 💎  By Kanixxx🥀`
 		await this.updateProfileStatus(bio).catch(_ => _)
 		setting.status = new Date() * 1
 
