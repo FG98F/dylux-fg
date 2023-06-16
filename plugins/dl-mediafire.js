677b4e4a065e64b682deaf0bb44c84a8e610cc88
FG98 <81941690+FG98F@users.noreply.github.com>
2023-06-16 16:24:07 -0300

    Add files via upload

--- plugins/dl-mediafire.js
+++ plugins/dl-mediafire.js
@@ -33,19 +33,19 @@ ${isLimit ? `\n▢ El archivo supera el límite de descarga *+${free} MB*\nPása
 
         try {
 	let res = await fg.mediafireDl(args[0])
-    let { link, name, ext, size, sizeN } = res
-   
-	let caption = `
-   ≡ *MEDIAFIRE DL 2*
-
-▢ *Nombre:* ${name}
-▢ *Tamaño:* ${size}
+     let { url, url2, filename, ext, upload_date, filesize, filesizeB } = res
+    let isLimit = limit * 1024 < filesizeB
+    let caption = `
+   ≡ *MEDIAFIRE*
+▢ *Nombre:* ${filename}
+▢ *Tamaño:* ${filesize}
 ▢ *Extension:* ${ext}
-`
-if (size.split('MB')[0] >= limit) return m.reply(`▢ El archivo supera el límite de descarga *+${free} MB*\nPásate a premium para poder descargar archivos más de *${prem} MB*`)
-if (size.includes('GB')) return m.reply(`▢ El archivo supera el límite de descarga *+${free} MB*\nPásate a premium para poder descargar archivos más de *${prem} MB*`)
+▢ *Subido:* ${upload_date}
+${isLimit ? `\n▢ El archivo supera el límite de descarga *+${free} MB*\nPásate a premium para poder descargar archivos más de *${prem} MB*` : ''} 
+`.trim()
+
 await conn.sendFile(m.chat, ss, 'ssweb.png', caption, m)
-await conn.sendFile(m.chat, link, name, '', m, null, { mimetype: ext, asDocument: true })
+if(!isLimit) await conn.sendFile(m.chat, url, filename, '', m, null, { mimetype: ext, asDocument: true })
     m.react(done)
 } catch {
     m.reply(`Error: intenta con otro link`)
