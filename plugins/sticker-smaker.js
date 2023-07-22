02af35f3e54fd53f87e1ee2f306dcf20710b629e
FG98 <81941690+FG98F@users.noreply.github.com>
2023-07-22 16:09:56 -0300

    Update sticker-smaker.js

--- plugins/sticker-smaker.js
+++ plugins/sticker-smaker.js
@@ -20,7 +20,7 @@ if (!mime) throw '✳️ Responde a una imagen'
 if (!/image\/(jpe?g|png)/.test(mime)) throw `✳️ Formato no soportado`
 let img = await q.download()
 let url = await uploadImage(img)
-let apiUrl = global.API('https://some-random-api.ml/canvas/', encodeURIComponent(effect), {
+let apiUrl = global.API('https://some-random-api.com/canvas/', encodeURIComponent(effect), {
 avatar: url
 })
 try {
