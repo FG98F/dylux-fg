baf56ad67750715d7e7c857f97be8bc68f9c5426
FG98 <81941690+FG98F@users.noreply.github.com>
2023-07-26 11:39:29 -0300

    Update tools-openai.js

--- plugins/tools-openai.js
+++ plugins/tools-openai.js
@@ -1,15 +1,14 @@
 
-import cheerio from 'cheerio'
-import gpt from 'api-dylux'
+import fetch from 'node-fetch'
 let handler = async (m, { conn, text }) => {
 	
 if (!text) throw `✳️ Ingresa el texto`
 m.react('💬')
 
 	try {
-        let syms = `Eres DyLux Bot, un gran modelo de lenguaje entrenado por OpenAI. Siga cuidadosamente las instrucciones del usuario. Responde usando Markdown.`
-        let res = await gpt.ChatGpt(text, syms)
-         await m.reply(res.text)
+        let gpt = await fetch(global.API('fgmods', '/api/info/openai2', { text }, 'apikey'));
+        let res = await gpt.json()
+        await m.reply(res.result)
 	} catch {
 		m.reply(`❎ Error: intenta más tarde`)
 	}
