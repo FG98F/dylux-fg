7ebc0797510c26d6cdedd18935f0e89ff37016ee
FG98 <81941690+FG98F@users.noreply.github.com>
2023-09-15 20:31:58 -0300

    Update dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -1,15 +1,16 @@
- 
+
 import fg from 'api-dylux'
 import fetch from 'node-fetch'
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
-    try {
+    
         if (!args[0]) throw `✳️ Ingrese un link de Tiktok\n\n 📌 Ejemplo : ${usedPrefix + command} https://vm.tiktok.com/ZMjkj76X6/`
         if (!args[0].match(/tiktok/gi)) throw `❎ verifica que el link sea de tiktok`
         m.react(rwait)
-
+      try {
         let res = await fetch(global.API('fgmods', '/api/downloader/tiktok2', { url: args[0] }, 'apikey'))
         let data = await res.json()
         
+        if (data.result.video) {
             let tex = `
 ┌─⊷ *TIKTOK DL*
 ▢ *Nombre:* ${data.result.author.name}
@@ -23,13 +24,24 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 `
             conn.sendFile(m.chat, data.result.video.noWatermark, 'tiktok.mp4', tex, m);
             m.react(done)
+   } else {
+            let cap = `
+▢ *Descripción:* ${data.result.title}
+▢ *Likes:* ${data.result.stats.likeCount}
+`
+            for (let tt of data.result.images) {
+                conn.sendMessage(m.chat, { image: { url: tt.url }, caption: cap }, { quoted: m })
+            }
+            conn.sendFile(m.chat, data.result.music.play_url, 'tiktok.mp3', '', m, null, { mimetype: 'audio/mp4' })
+            m.react(done)
+        }
     } catch (error) {
         m.reply(`❎ Error al descargar el video`)
     }
 }
 handler.help = ['tiktok']
 handler.tags = ['dl']
-handler.command = ['tiktok', 'tt', 'tiktodl']
+handler.command = ['tiktok', 'tt', 'tiktokimg', 'tiktokslide']
 handler.diamond = true
 
 export default handler
